d875ba073832421d4938dbdd39025113aff133dd
Rias <hello@rias.be>
2020-02-17 14:37:14 +0100

    🚧

---
+++ tests/TestClasses/TestCustomInstanciatedQueryOnlyShouldSendToJohn.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Spatie\Mailcoach\Tests\TestClasses;
+
+use Spatie\Mailcoach\Models\Subscriber;
+use Spatie\Mailcoach\Support\Segments\Segment;
+
+class TestCustomInstanciatedQueryOnlyShouldSendToJohn extends Segment
+{
+    public string $email;
+
+    public function __construct(string $email)
+    {
+        $this->email = $email;
+    }
+
+    public function shouldSend(Subscriber $subscriber): bool
+    {
+        return $subscriber->email === $this->email;
+    }
+
+    public function description(): string
+    {
+        return 'only to john';
+    }
+}
