9d4f7639dd8ce333033a7c80021916637d426b8e
freek <freek@spatie.be>
2020-03-20 12:56:30 +0100

    fix group by of campaign opens query

--- src/Http/App/Queries/CampaignOpensQuery.php
+++ src/Http/App/Queries/CampaignOpensQuery.php
@@ -5,6 +5,7 @@ namespace Spatie\Mailcoach\Http\App\Queries;
 use Spatie\Mailcoach\Http\App\Queries\Filters\FuzzyFilter;
 use Spatie\Mailcoach\Models\Campaign;
 use Spatie\Mailcoach\Models\CampaignOpen;
+use Spatie\Mailcoach\Models\Subscriber;
 use Spatie\QueryBuilder\AllowedFilter;
 use Spatie\QueryBuilder\QueryBuilder;
 
@@ -16,14 +17,13 @@ class CampaignOpensQuery extends QueryBuilder
     {
         $query = CampaignOpen::query()
             ->selectRaw('
-                mailcoach_subscribers.id as subscriber_id,
+                mailcoach_campaign_opens.subscriber_id as subscriber_id,
                 mailcoach_subscribers.email as subscriber_email,
-                count(mailcoach_subscribers.id) as open_count,
+                count(mailcoach_campaign_opens.subscriber_id) as open_count,
                 min(mailcoach_campaign_opens.created_at) AS first_opened_at
             ')
-            ->join('mailcoach_sends', 'mailcoach_sends.id', '=', 'mailcoach_campaign_opens.send_id')
-            ->join('mailcoach_campaigns', 'mailcoach_campaigns.id', '=', 'mailcoach_sends.campaign_id')
-            ->join('mailcoach_subscribers', 'mailcoach_subscribers.id', '=', 'mailcoach_sends.subscriber_id')
+            ->join('mailcoach_campaigns', 'mailcoach_campaigns.id', '=', 'mailcoach_campaign_opens.campaign_id')
+            ->join('mailcoach_subscribers', 'mailcoach_subscribers.id', '=', 'mailcoach_campaign_opens.subscriber_id')
             ->where('mailcoach_campaigns.id', $campaign->id);
 
         $this->totalCount = $query->count();
@@ -33,7 +33,7 @@ class CampaignOpensQuery extends QueryBuilder
         $this
             ->defaultSort('-first_opened_at')
             ->allowedSorts('email', 'open_count', 'first_opened_at')
-            ->groupBy('mailcoach_subscribers.id')
+            ->groupBy('mailcoach_campaign_opens.subscriber_id', 'mailcoach_subscribers.email')
             ->allowedFilters(
                 AllowedFilter::custom(
                     'search',
