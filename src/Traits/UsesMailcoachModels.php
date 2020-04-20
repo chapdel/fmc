22d35dde85dcaf4ec8862694d82c782471eff66a
Jeremy Braband <jbraband+github@gmail.com>
2020-04-20 17:43:00 +0000

    set default values for the configs

--- src/Traits/UsesMailcoachModels.php
+++ src/Traits/UsesMailcoachModels.php
@@ -6,16 +6,16 @@ trait UsesMailcoachModels
 {
     public function getCampaignClass(): string
     {
-        return config('mailcoach.models.campaign');
+        return config('mailcoach.models.campaign', \Spatie\Mailcoach\Models\Campaign::class);
     }
 
     public function getEmailListClass(): string
     {
-        return config('mailcoach.models.email_list');
+        return config('mailcoach.models.email_list', \Spatie\Mailcoach\Models\EmailList::class);
     }
 
     public function getSubscriberClass(): string
     {
-        return config('mailcoach.models.campaign');
+        return config('mailcoach.models.campaign', \Spatie\Mailcoach\Models\Subscriber::class);
     }
 }
