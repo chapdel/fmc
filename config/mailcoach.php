dd61bf049711877f1b4446b36e5fc07ddd10fc79
freek <freek@spatie.be>
2020-04-24 18:45:10 +0200

    wip

--- config/mailcoach.php
+++ config/mailcoach.php
@@ -137,24 +137,21 @@ return [
     'models' => [
 
         /*
-         * The model you want to use as a Campaign model needs to extend the
-         * `Spatie\Mailcoach\Models\Campaign` model.
+         * The model you want to use as a Campaign model. It needs to be or
+         * extend the `Spatie\Mailcoach\Models\Campaign` model.
          */
-
         'campaign' => Spatie\Mailcoach\Models\Campaign::class,
 
         /*
-         * The model you want to use as a EmailList model needs to extend the
-         * `Spatie\Mailcoach\Models\EmailList` model.
+         * The model you want to use as a EmailList model. It needs to be or
+         * extend the `Spatie\Mailcoach\Models\EmailList` model.
          */
-
         'email_list' => Spatie\Mailcoach\Models\EmailList::class,
 
         /*
-         * The model you want to use as a Subscriber model needs to extend the
-         * `Spatie\Mailcoach\Models\Subscriber` model.
+         * The model you want to use as a Subscriber model. It needs to be or
+         * extend the `Spatie\Mailcoach\Models\Subscriber` model.
          */
-
         'subscriber' => Spatie\Mailcoach\Models\Subscriber::class,
 
     ],
--- src/Traits/UsesMailcoachModels.php
+++ src/Traits/UsesMailcoachModels.php
@@ -2,20 +2,24 @@
 
 namespace Spatie\Mailcoach\Traits;
 
+use Spatie\Mailcoach\Models\Campaign;
+use Spatie\Mailcoach\Models\EmailList;
+use Spatie\Mailcoach\Models\Subscriber;
+
 trait UsesMailcoachModels
 {
     public function getCampaignClass(): string
     {
-        return config('mailcoach.models.campaign', \Spatie\Mailcoach\Models\Campaign::class);
+        return config('mailcoach.models.campaign', Campaign::class);
     }
 
     public function getEmailListClass(): string
     {
-        return config('mailcoach.models.email_list', \Spatie\Mailcoach\Models\EmailList::class);
+        return config('mailcoach.models.email_list', EmailList::class);
     }
 
     public function getSubscriberClass(): string
     {
-        return config('mailcoach.models.subscriber', \Spatie\Mailcoach\Models\Subscriber::class);
+        return config('mailcoach.models.subscriber', Subscriber::class);
     }
 }
