17ff33cd92a42a6e0acef3cf46141f2e5753df02
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2020-05-04 14:48:12 +0200

    Transition async await refactor

--- resources/js/util/transition.js
+++ resources/js/util/transition.js
@@ -1,55 +1,55 @@
-export function enter(element, transitionName = 'transition') {
+export async function enter(element, transitionName) {
+    element.classList.remove('hidden');
+
+    element.classList.add(`${transitionName}-enter`);
+    element.classList.add(`${transitionName}-enter-start`);
+
+    await nextFrame();
+
+    element.classList.remove(`${transitionName}-enter-start`);
+    element.classList.add(`${transitionName}-enter-end`);
+
+    await afterTransition(element);
+
+    element.classList.remove(`${transitionName}-enter-end`);
+    element.classList.remove(`${transitionName}-enter`);
+
+    await nextFrame();
+}
+
+export async function leave(element, transitionName) {
+    element.classList.add(`${transitionName}-leave`);
+    element.classList.add(`${transitionName}-leave-start`);
+
+    await nextFrame();
+
+    element.classList.remove(`${transitionName}-leave-start`);
+    element.classList.add(`${transitionName}-leave-end`);
+
+    await afterTransition(element);
+
+    element.classList.remove(`${transitionName}-leave-end`);
+    element.classList.remove(`${transitionName}-leave`);
+
+    element.classList.add('hidden');
+
+    await nextFrame();
+}
+
+function afterTransition(element) {
     return new Promise(resolve => {
-        element.classList.remove('hidden');
+        const duration = Number(getComputedStyle(element).transitionDuration.replace('s', '')) * 1000;
 
-        element.classList.add(`${transitionName}-enter`);
-        element.classList.add(`${transitionName}-enter-start`);
-
-        nextFrame(() => {
-            element.classList.remove(`${transitionName}-enter-start`);
-            element.classList.add(`${transitionName}-enter-end`);
-
-            afterTransition(element, () => {
-                element.classList.remove(`${transitionName}-enter-end`);
-                element.classList.remove(`${transitionName}-enter`);
-
-                nextFrame(() => {
-                    resolve(element);
-                });
-            });
-        });
+        setTimeout(() => {
+            resolve();
+        }, duration);
     });
 }
 
-export function leave(element, transitionName = 'transition') {
+function nextFrame() {
     return new Promise(resolve => {
-        element.classList.add(`${transitionName}-leave`);
-        element.classList.add(`${transitionName}-leave-start`);
-
-        nextFrame(() => {
-            element.classList.remove(`${transitionName}-leave-start`);
-            element.classList.add(`${transitionName}-leave-end`);
-
-            afterTransition(element, () => {
-                element.classList.remove(`${transitionName}-leave-end`);
-                element.classList.remove(`${transitionName}-leave`);
-
-                element.classList.add('hidden');
-
-                nextFrame(() => {
-                    resolve(element);
-                });
-            });
+        requestAnimationFrame(() => {
+            requestAnimationFrame(resolve);
         });
     });
-}
-
-function afterTransition(element, callback) {
-    const duration = Number(getComputedStyle(element).transitionDuration.replace('s', '')) * 1000;
-
-    setTimeout(callback, duration);
-}
-
-function nextFrame(callback) {
-    requestAnimationFrame(() => requestAnimationFrame(callback));
 }
