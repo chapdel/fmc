23d9d842b6a79bfb265634e66aa4608b291782ad
Rias <hello@rias.be>
2020-05-27 15:15:00 +0200

    nullable

--- src/Http/App/Requests/StoreCampaignRequest.php
+++ src/Http/App/Requests/StoreCampaignRequest.php
@@ -11,7 +11,7 @@ class StoreCampaignRequest extends FormRequest
     {
         return [
             'name' => 'required',
-            'email_list_id' => 'required',
+            'email_list_id' => 'nullable',
         ];
     }
 
