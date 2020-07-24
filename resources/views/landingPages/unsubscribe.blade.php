1b14d9d71f3f29ddd040c3b2b254823b63edd6e4
freek <freek@spatie.be>
2020-07-24 10:08:36 +0200

    wip

--- resources/views/landingPages/unsubscribe.blade.php
+++ resources/views/landingPages/unsubscribe.blade.php
@@ -1,17 +1,14 @@
 @extends('mailcoach::landingPages.layouts.landingPage', ['title' => __('Unsubscribed')])
 
 @section('content')
-    <p>
-        {{ __('Do you want to unsubscribe?') }}
-    </p>
     <p class="mt-4">
-        {!! __('Are you sure you want to unsubscribe from list <strong class="font-semibold">:emailListName</strong>.', ['emailListName' => $subscriber->emailList->name]) !!}
+        {!! __('Are you sure you want to unsubscribe from list <strong class="font-semibold">:emailListName</strong>?', ['emailListName' => $subscriber->emailList->name]) !!}
     </p>
-    <div>
+
+    <div class="mt-4">
         <form method="POST">
             @csrf
-
-            <button id="confirmationButton" type="submit">{{ !! __('Confirm unsubscribe') }}</button>
+            <button class="button bg-red-400 shadow" id="confirmationButton" type="submit">{{__('Unsubscribe') }}</button>
         </form>
     </div>
 
