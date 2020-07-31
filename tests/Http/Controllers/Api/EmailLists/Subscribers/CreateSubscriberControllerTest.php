52e3e47d71ad769a2077ac23f727337a54618b79
freek <freek@spatie.be>
2020-08-01 01:12:13 +0200

    wip

--- tests/Http/Controllers/Api/EmailLists/Subscribers/CreateSubscriberControllerTest.php
+++ tests/Http/Controllers/Api/EmailLists/Subscribers/CreateSubscriberControllerTest.php
@@ -2,8 +2,12 @@
 
 namespace Spatie\Mailcoach\Tests\Http\Controllers\Api\EmailLists\Subscribers;
 
+use Illuminate\Support\Facades\Mail;
+use Spatie\Mailcoach\Enums\SubscriptionStatus;
 use Spatie\Mailcoach\Http\Api\Controllers\EmailLists\Subscribers\SubscribersController;
+use Spatie\Mailcoach\Mails\ConfirmSubscriberMail;
 use Spatie\Mailcoach\Models\EmailList;
+use Spatie\Mailcoach\Models\Subscriber;
 use Spatie\Mailcoach\Tests\Http\Controllers\Api\Concerns\RespondsToApiRequests;
 use Spatie\Mailcoach\Tests\TestCase;
 
@@ -13,29 +17,55 @@ class CreateSubscriberControllerTest extends TestCase
 
     private EmailList $emailList;
 
+    private array $attributes;
+
     public function setUp(): void
     {
         parent::setUp();
 
         $this->loginToApi();
 
-        $this->emailList = factory(EmailList::class)->create();
-    }
+        $this->emailList = factory(EmailList::class)->create([
+            'requires_confirmation' => true,
+        ]);
 
-    /** @test */
-    public function it_can_create_a_subscriber_using_the_api()
-    {
-        $attributes = [
+        $this->attributes = [
             'email_list_id' => $this->emailList->id,
             'email' => 'john@example.com',
             'first_name' => 'John',
             'last_name' => 'Doe',
         ];
 
+        Mail::fake();
+    }
+
+    /** @test */
+    public function it_can_create_a_subscriber_using_the_api()
+    {
+        $this
+            ->postJson(action([SubscribersController::class, 'store'], $this->emailList), $this->attributes)
+            ->assertSuccessful();
+
+        $this->assertDatabaseHas('mailcoach_subscribers', $this->attributes);
+
+        $this->assertEquals(SubscriptionStatus::UNCONFIRMED, Subscriber::first()->status);
+
+        Mail::assertQueued(ConfirmSubscriberMail::class);
+    }
+
+    /** @test */
+    public function it_can_skip_the_confirmation_while_subscribing()
+    {
+        $attributes = $this->attributes;
+
+        $attributes['skip_confirmation'] = true;
+
         $this
             ->postJson(action([SubscribersController::class, 'store'], $this->emailList), $attributes)
             ->assertSuccessful();
 
-        $this->assertDatabaseHas('mailcoach_subscribers', $attributes);
+        $this->assertEquals(SubscriptionStatus::SUBSCRIBED, Subscriber::first()->status);
+
+        Mail::assertNotQueued(ConfirmSubscriberMail::class);
     }
 }
