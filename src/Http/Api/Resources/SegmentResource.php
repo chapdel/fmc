ab0c4d057f14057d74c5f9cb57ffdf0379226b79
Rias <hey@rias.be>
2020-08-06 13:58:07 +0200

    Add IDs to the API resources

--- src/Http/Api/Resources/SegmentResource.php
+++ src/Http/Api/Resources/SegmentResource.php
@@ -9,6 +9,7 @@ class SegmentResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
 
             'created_at' => $this->created_at,
