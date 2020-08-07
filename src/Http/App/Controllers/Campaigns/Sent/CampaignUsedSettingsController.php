e6a0d8fe09dc17a2191eaf316d43984afb10a4e8
riasvdv <riasvdv@users.noreply.github.com>
2020-08-07 13:42:27 +0000

    fix code style

--- src/Http/App/Controllers/Campaigns/Sent/CampaignUsedSettingsController.php
+++ src/Http/App/Controllers/Campaigns/Sent/CampaignUsedSettingsController.php
@@ -2,10 +2,7 @@
 
 namespace Spatie\Mailcoach\Http\App\Controllers\Campaigns\Sent;
 
-use Spatie\Mailcoach\Http\App\Requests\UpdateCampaignSettingsRequest;
 use Spatie\Mailcoach\Models\Campaign;
-use Spatie\Mailcoach\Models\EmailList;
-use Spatie\Mailcoach\Traits\UsesMailcoachModels;
 
 class CampaignUsedSettingsController
 {
