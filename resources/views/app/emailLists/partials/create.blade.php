93c29925bd9b6b56eef9c39117fc3be426f6a2b7
Rias <hey@rias.be>
2020-08-11 09:08:23 +0200

    Add some placeholders

--- resources/views/app/emailLists/partials/create.blade.php
+++ resources/views/app/emailLists/partials/create.blade.php
@@ -1,9 +1,9 @@
 <form class="form-grid" action="{{ route('mailcoach.emailLists.store') }}" method="POST">
     @csrf
 
-    <x-text-field :label="__('Name')"  name="name" required />
-    <x-text-field :label="__('From email')" name="default_from_email" type="email" required />
-    <x-text-field :label="__('From name')" name="default_from_name" />
+    <x-text-field :label="__('Name')"  name="name" :placeholder="__('Subscribers')" required />
+    <x-text-field :label="__('From email')" :placeholder="auth()->user()->email" name="default_from_email" type="email" required />
+    <x-text-field :label="__('From name')" :placeholder="auth()->user()->name" name="default_from_name" />
 
     <div class="form-buttons">
         <button class="button">
