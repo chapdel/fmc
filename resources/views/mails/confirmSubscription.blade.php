fdf71b24ebd79f8232d80900171fb59684577ff2
freek <freek@spatie.be>
2020-10-13 14:50:17 +0200

    fixed extra . in subscription confirmation

--- resources/views/mails/confirmSubscription.blade.php
+++ resources/views/mails/confirmSubscription.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
 {{ __('Hey') }},
 
-{{ __('You are almost subscribed to the list **:emailListName**.', ['emailListName'=>$subscriber->emailList->name]) }}.
+{{ __('You are almost subscribed to the list **:emailListName**.', ['emailListName'=>$subscriber->emailList->name]) }}
 
 {{ __('Prove it is really you by pressing the button below') }}.
 
