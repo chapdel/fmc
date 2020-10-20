27d8dd950639516ac33ec7548e8037f9090640b8
Hugo Clarke-Wing <7689302+clarkewing@users.noreply.github.com>
2020-10-20 16:50:44 +0200

    Disable autocomplete on search inputs
    To make the search experience slightly less frustrating, we can disable browser autocomplete, autocorrect, autocapitalize, and spellcheck on search inputs.

--- resources/views/app/components/search.blade.php
+++ resources/views/app/components/search.blade.php
@@ -1,6 +1,7 @@
 {{-- [data-turbolinks-permanent] is necessary to preserve the cursor state during focus --}}
 <div class="search">
     <input type="search" required placeholder="{{ $placeholder }}" value="{{ $value }}"
+        autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false"
         id="turbolinks-search-{{ Illuminate\Support\Str::slug($queryString->disable('filter[search]')) }}"
         data-turbolinks-permanent
         data-turbolinks-search
