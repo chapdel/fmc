2deae576d00994e0cd5a0ecd14ccd0bbfe3c19a0
riasvdv <riasvdv@users.noreply.github.com>
2020-12-24 09:26:08 +0000

    Fix styling

--- src/Domain/Campaign/Listeners/AddCampaignOpenedTag.php
+++ src/Domain/Campaign/Listeners/AddCampaignOpenedTag.php
@@ -2,11 +2,8 @@
 
 namespace Spatie\Mailcoach\Domain\Campaign\Listeners;
 
-use Illuminate\Support\Facades\Mail;
 use Spatie\Mailcoach\Domain\Campaign\Enums\TagType;
 use Spatie\Mailcoach\Domain\Campaign\Events\CampaignOpenedEvent;
-use Spatie\Mailcoach\Domain\Campaign\Events\CampaignSentEvent;
-use Spatie\Mailcoach\Domain\Campaign\Mails\CampaignSentMail;
 
 class AddCampaignOpenedTag
 {
