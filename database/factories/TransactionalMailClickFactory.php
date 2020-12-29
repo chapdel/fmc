54d9f55f07ff2036e1c3d2970f331676bd827333
freekmurze <freekmurze@users.noreply.github.com>
2020-12-29 09:53:16 +0000

    Fix styling

--- database/factories/TransactionalMailClickFactory.php
+++ database/factories/TransactionalMailClickFactory.php
@@ -4,7 +4,6 @@ namespace Spatie\Mailcoach\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Spatie\Mailcoach\Domain\Campaign\Models\Send;
-use Spatie\Mailcoach\Domain\TransactionalMail\Models\TransactionalMail;
 use Spatie\Mailcoach\Domain\TransactionalMail\Models\TransactionalMailClick;
 
 class TransactionalMailClickFactory extends Factory
