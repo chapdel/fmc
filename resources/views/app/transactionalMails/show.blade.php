671609c70da3db1a06fdb55ba997d2e1a91fc580
freek <freek@spatie.be>
2020-12-29 11:36:26 +0100

    wip

--- resources/views/app/transactionalMails/show.blade.php
+++ resources/views/app/transactionalMails/show.blade.php
@@ -5,31 +5,50 @@
 @endsection
 
 @section('transactionalMail')
-    Subject: {{ $transactionalMail->subject }}<br />
+    Subject: {{ $transactionalMail->subject }}<br/>
 
-    <x-mailcoach::mail-persons label="From" :persons="$transactionalMail->from" />
-    <x-mailcoach::mail-persons label="To" :persons="$transactionalMail->to" />
-    <x-mailcoach::mail-persons label="Cc" :persons="$transactionalMail->cc" />
-    <x-mailcoach::mail-persons label="Bcc" :persons="$transactionalMail->bcc" />
+    <x-mailcoach::mail-persons label="From" :persons="$transactionalMail->from"/>
+    <x-mailcoach::mail-persons label="To" :persons="$transactionalMail->to"/>
+    <x-mailcoach::mail-persons label="Cc" :persons="$transactionalMail->cc"/>
+    <x-mailcoach::mail-persons label="Bcc" :persons="$transactionalMail->bcc"/>
 
-    <iframe width="560" height="315" src="{{ route('mailcoach.transactionalMail.body', $transactionalMail) }}" />
+    <iframe width="560" height="315" src="{{ route('mailcoach.transactionalMail.body', $transactionalMail) }}"/>
 
     <h3>Opens</h3>
     <ul>
-    @forelse($transactionalMail->opens as $open)
+        @forelse($transactionalMail->opens as $open)
             <li>{{ $open->created_at }}</li>
-    @empty
-        This mail hasn't been opened yet.
-    @endforelse
+        @empty
+            This mail hasn't been opened yet.
+        @endforelse
     </ul>
 
     <h3>Clicks</h3>
     <ul>
-        @forelse($transactionalMail->clicks as $click)
-            <li>{{ $open->created_at }}</li>
-        @empty
+        @if($transactionalMail->clicksPerUrl()->count())
+            <table>
+                <thead>
+                <tr>
+                    <td>URL</td>
+                    <td>Click count</td>
+                    <td>First clicked at</td>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($transactionalMail->clicksPerUrl() as $clickGroup)
+                    <tr>
+                        <td>{{ $clickGroup['url'] }}</td>
+                        <td>{{ $clickGroup['count'] }}</td>
+                        <td>{{ $clickGroup['first_clicked_at'] }}</td>
+                    </tr>
+                @endforeach
+                </tbody>
+
+            </table>
+        @else
             No links in this mail have been clicked yet.
-        @endforelse
+        @endif
+
     </ul>
 
 
