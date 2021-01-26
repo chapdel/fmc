386508d1dbaa7f0eb743aff4316d0e63dcb8c55d
riasvdv <riasvdv@users.noreply.github.com>
2021-01-26 11:51:40 +0000

    Fix styling

--- src/Http/Front/Controllers/UnsubscribeTagController.php
+++ src/Http/Front/Controllers/UnsubscribeTagController.php
@@ -3,7 +3,6 @@
 namespace Spatie\Mailcoach\Http\Front\Controllers;
 
 use Spatie\Mailcoach\Domain\Campaign\Enums\SubscriptionStatus;
-use Spatie\Mailcoach\Domain\Campaign\Models\Send;
 use Spatie\Mailcoach\Domain\Shared\Traits\UsesMailcoachModels;
 
 class UnsubscribeTagController
