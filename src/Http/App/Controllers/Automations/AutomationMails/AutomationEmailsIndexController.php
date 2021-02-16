dede6152fd9ab181db1f1cf7a0c007758a4fd599
freekmurze <freekmurze@users.noreply.github.com>
2021-02-16 11:29:24 +0000

    Fix styling

--- src/Http/App/Controllers/Automations/AutomationMails/AutomationEmailsIndexController.php
+++ src/Http/App/Controllers/Automations/AutomationMails/AutomationEmailsIndexController.php
@@ -5,7 +5,6 @@ namespace Spatie\Mailcoach\Http\App\Controllers\Automations\AutomationMails;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Spatie\Mailcoach\Domain\Audience\Models\EmailList;
 use Spatie\Mailcoach\Domain\Automation\Models\AutomationMail;
-use Spatie\Mailcoach\Domain\Campaign\Models\Campaign;
 use Spatie\Mailcoach\Domain\Campaign\Models\Template;
 use Spatie\Mailcoach\Domain\Shared\Traits\UsesMailcoachModels;
 use Spatie\Mailcoach\Http\App\Queries\CampaignsQuery;
