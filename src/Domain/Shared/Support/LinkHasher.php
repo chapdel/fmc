d027a886744612fa0e03c95101fa8a46d7ae57ed
riasvdv <riasvdv@users.noreply.github.com>
2021-03-09 12:17:21 +0000

    Fix styling

--- src/Domain/Shared/Support/LinkHasher.php
+++ src/Domain/Shared/Support/LinkHasher.php
@@ -5,7 +5,6 @@ namespace Spatie\Mailcoach\Domain\Shared\Support;
 use Illuminate\Support\Str;
 use Spatie\Mailcoach\Domain\Automation\Models\AutomationMail;
 use Spatie\Mailcoach\Domain\Campaign\Models\Campaign;
-use Spatie\Mailcoach\Domain\Campaign\Models\Template;
 use Spatie\Mailcoach\Domain\Shared\Models\Sendable;
 
 class LinkHasher
