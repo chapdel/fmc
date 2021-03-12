8113ba29b2616ad24cf038dba25edc2b0bf26259
Rias <hey@rias.be>
2021-03-12 14:19:19 +0100

    Url encode

--- src/Domain/Shared/Actions/AddUtmTagsToUrlAction.php
+++ src/Domain/Shared/Actions/AddUtmTagsToUrlAction.php
@@ -6,6 +6,7 @@ class AddUtmTagsToUrlAction
 {
     public function execute(string $url, string $campaignName): string
     {
+        $campaignName = urlencode($campaignName);
         $utmTags = "utm_source=newsletter&utm_medium=email&utm_campaign={$campaignName}";
 
         return $url . (parse_url($url, PHP_URL_QUERY) ? '&' : '?') . $utmTags;
