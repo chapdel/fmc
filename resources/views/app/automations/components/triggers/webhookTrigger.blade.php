c1e9b851a2ef52fc0da9b781f01d1f1ab1c8a328
Rias <hey@rias.be>
2021-03-15 15:56:59 +0100

    Fix formatting

--- resources/views/app/automations/components/triggers/webhookTrigger.blade.php
+++ resources/views/app/automations/components/triggers/webhookTrigger.blade.php
@@ -3,12 +3,12 @@
     <p class="max-w-full overflow-x-auto bg-blue-100 p-2"><code class="whitespace-nowrap">{{ action(\Spatie\Mailcoach\Http\Api\Controllers\Automations\TriggerAutomationController::class, [$this->automation]) }}</code></p>
     <p class="mt-4">Example POST request:</p>
     <pre class="max-w-full overflow-x-auto bg-blue-100 p-2">
-        <code class="">$ MAILCOACH_TOKEN="your API token"
-        $ curl -x POST {{ action(\Spatie\Mailcoach\Http\Api\Controllers\Automations\TriggerAutomationController::class, [$this->automation]) }} \
-            -H "Authorization: Bearer $MAILCOACH_TOKEN" \
-            -H 'Accept: application/json' \
-            -H 'Content-Type: application/json'
-            -d '{"subscribers":[1, 2, 3]}'
-        </code></pre>
+<code class="">$ MAILCOACH_TOKEN="your API token"
+$ curl -x POST {{ action(\Spatie\Mailcoach\Http\Api\Controllers\Automations\TriggerAutomationController::class, [$this->automation]) }} \
+    -H "Authorization: Bearer $MAILCOACH_TOKEN" \
+    -H 'Accept: application/json' \
+    -H 'Content-Type: application/json'
+    -d '{"subscribers":[1, 2, 3]}'
+</code></pre>
     <p class="my-4">The automation will only trigger for subscribed subscribers of the automation's email list & segment.</p>
 </div>
