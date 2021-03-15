f4dc4f15f70287337589f612b75c4838f9722358
Rias <hey@rias.be>
2021-03-15 16:04:00 +0100

    wip

--- resources/views/app/automations/components/triggers/noTrigger.blade.php
+++ resources/views/app/automations/components/triggers/noTrigger.blade.php
@@ -1,14 +1,8 @@
 <div class="form-field">
     <p>You can run the automation manually using code:</p>
     <pre class="max-w-full overflow-x-auto bg-blue-100 p-2">
-<code class="">// A single Subscriber instance
+<code class="">
 $automation->run($subscriber);
-
-// A collection or array containing Subscribers
-$automation->run($subscribers);
-
-// A query builder instance
-$automation->run($automation->emailList->subscribers());
 </code></pre>
     <p class="my-4">The automation will only run for subscribed subscribers of the automation's email list & segment.</p>
 </div>
