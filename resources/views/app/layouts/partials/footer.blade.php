83dcec2db8f121b418230bea8db04c0b4051c610
Willem Van Bockstal <willem@spatie.be>
2021-03-18 14:20:36 +0100

    Updates https://github.com/spatie/mailcoach-v4-test-app/issues/14

--- resources/views/app/layouts/partials/footer.blade.php
+++ resources/views/app/layouts/partials/footer.blade.php
@@ -1,28 +1,30 @@
 <div class="flex flex-wrap items-center justify-center text-gray-600 text-xs">
-    <a class="link-dimmed" href="https://spatie.be/docs/laravel-mailcoach" target="_blank">{{ __('Documentation') }}</a>
-    <span class="mx-2">•</span>
     <a class="link-dimmed inline-block truncate" style="max-width: 12rem" href="https://mailcoach.app">
         Mailcoach {{ $versionInfo->getCurrentVersion() }}
     </a>
     <span>&nbsp;{{ __('by') }} <a class="link-dimmed" href="https://spatie.be">SPATIE</a></span>
 
     @if(Auth::check())
-    <span class="mx-2">•</span>
-    <a class="link-dimmed inline-block truncate" style="max-width: 12rem" href="{{ route('debug') }}">
-        Debug
-    </a>
+        <span class="mx-2">•</span>
+        <a class="link-dimmed" href="https://spatie.be/docs/laravel-mailcoach" target="_blank">{{ __('Documentation') }}</a>
+        
+        <span class="mx-2">•</span>
+        <a class="link-dimmed inline-block truncate" style="max-width: 12rem" href="{{ route('debug') }}">
+            Debug
+        </a>
+        
+        @if(! $versionInfo->isLatest())
+            <a class="ml-4 my-2 inline-flex items-center bg-gray-400 bg-opacity-25 text-gray-600 rounded-sm px-2 leading-loose" href="/">
+                <i class="fas fa-horse-head opacity-75 mr-1"></i>
+                {{ __('Upgrade available') }}
+            </a>
+        @endif
     @endif
 
-    @if(! $versionInfo->isLatest())
-        <a class="ml-4 my-2 inline-flex items-center bg-gray-400 bg-opacity-25 text-gray-600 rounded-sm px-2 leading-loose" href="/">
-            <i class="far fa-horse-head opacity-75 mr-1"></i>
-            {{ __('Upgrade available') }}
-        </a>
-    @endif
 
     @if (! app()->environment('production') || config('app.debug'))
         <span class="ml-4 my-2 inline-flex items-center bg-gray-400 bg-opacity-25 text-red-600 rounded-sm px-2 leading-loose">
-            <i class="far fa-wrench opacity-75 mr-1"></i>
+            <i class="fas fa-wrench opacity-75 mr-1"></i>
             Env: {{ app()->environment() }} &mdash; Debug: {{ config('app.debug') ? 'true' : 'false' }}
         </span>
     @endif
