368df389d22406831c78b53027e4a21acfeb60f4
Rias <hey@rias.be>
2021-03-19 13:16:33 +0100

    Fix mail components

--- resources/views/mails/reconfirmSubscription.blade.php
+++ resources/views/mails/reconfirmSubscription.blade.php
@@ -1,11 +1,11 @@
-@component('mail::message')
+@component('mailcoach::mails.layout.message')
 {{ __('Hey') }},
 
 {{ __('It seems like you haven’t read our emails in a while.') }}
 
 {{ __('Do you want to stay subscribed to our email list **:emailListName**?', ['emailListName'=>$subscriber->emailList->name]) }}
 
-@component('mail::button', ['url' => $confirmationUrl])
+@component('mailcoach::mails.layout.button', ['url' => $confirmationUrl])
     {{ __('Stay subscribed') }}
 @endcomponent
 
