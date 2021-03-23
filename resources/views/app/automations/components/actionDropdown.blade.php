1654e3f9e84b383eb7218358b2f8354031567829
Willem Van Bockstal <willem@spatie.be>
2021-03-23 11:54:45 +0100

    Closes https://github.com/spatie/mailcoach.app/issues/73

--- resources/views/app/automations/components/actionDropdown.blade.php
+++ resources/views/app/automations/components/actionDropdown.blade.php
@@ -1,8 +1,12 @@
 <div class="flex my-6">
-    <div class="w-64 -ml-6 pl-6 h-0 flex justify-start items-center border-t border-dashed border-gray-500 border-opacity-25">
+    <div class="-ml-6 pl-6 w-64 flex">
+        <hr class="absolute left-0 w-full h-0 top-1/2 transform -translate-y-1/2 border-t border-dashed border-gray-500 border-opacity-25">
     <x-mailcoach::dropdown direction="right">
         <x-slot name="trigger">
-                <div class="button-rounded" title="{{__('Insert action')}}">+</div>
+            <div class="group button-rounded" title="{{__('Insert action')}}">
+                <span class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-2 h-px bg-gray-600 group-hover:bg-yellow-700"></span>
+                <span class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-px h-2 bg-gray-600 group-hover:bg-yellow-700"></span>
+            </div>
         </x-slot>
 
         <div style="min-width:40rem" class="max-w-full px-1 py-4 grid items-start gap-8 grid-cols-2">
