94bab86fbd7817bcad745f0bb8f919480d558585
riasvdv <riasvdv@users.noreply.github.com>
2022-03-09 08:59:42 +0000

    Fix styling

--- tests/Http/Controllers/Api/EmailLists/Subscribers/SubscribersControllerTest.php
+++ tests/Http/Controllers/Api/EmailLists/Subscribers/SubscribersControllerTest.php
@@ -99,7 +99,7 @@ it('wont confuse id and uuid', function () {
     /** @var Subscriber $subscriber1 */
     $subscriber1 = Subscriber::factory()->create([
         'id' => 1,
-        'uuid' => '150ecdda-14b8-49aa-b1a1-6a07d42c2a76'
+        'uuid' => '150ecdda-14b8-49aa-b1a1-6a07d42c2a76',
     ]);
 
     Subscriber::factory()->create([
