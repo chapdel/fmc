d9a36c4d055207b50ff5736e389ea4544e8e8ba7
Rias <hey@rias.be>
2022-03-09 10:07:53 +0100

    wip

--- tests/Http/Controllers/Api/EmailLists/Subscribers/SubscribersControllerTest.php
+++ tests/Http/Controllers/Api/EmailLists/Subscribers/SubscribersControllerTest.php
@@ -98,7 +98,6 @@ it('can show a subscriber by uuid', function () {
 it('wont confuse id and uuid', function () {
     /** @var Subscriber $subscriber1 */
     $subscriber1 = Subscriber::factory()->create([
-        'id' => 1,
         'uuid' => '150ecdda-14b8-49aa-b1a1-6a07d42c2a76',
     ]);
 
