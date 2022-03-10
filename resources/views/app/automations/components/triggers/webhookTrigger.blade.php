fafa090de85423f915c343a59155c72489abc2f3
Rias <hey@rias.be>
2022-03-10 09:08:52 +0100

    Fix webhooktrigger controller - fix #907

--- resources/views/app/automations/components/triggers/webhookTrigger.blade.php
+++ resources/views/app/automations/components/triggers/webhookTrigger.blade.php
@@ -1,10 +1,10 @@
 <div class="form-field">
     <p>Send an authenticated <code>POST</code> request to the following endpoint with an array of subscriber ids, make sure you've set up the <a class="link" href="https://spatie.be/docs/laravel-mailcoach/v4/api/introduction">Mailcoach API</a>.</p>
-    <p class="max-w-full overflow-x-auto bg-blue-100 p-2"><code class="whitespace-nowrap">{{ action(\Spatie\Mailcoach\Http\Api\Controllers\Automations\TriggerAutomationController::class, [$this->automation]) }}</code></p>
+    <p class="max-w-full overflow-x-auto bg-blue-100 p-2"><code class="whitespace-nowrap">{{ action('\\' . \Spatie\Mailcoach\Http\Api\Controllers\Automations\TriggerAutomationController::class, [$this->automation]) }}</code></p>
     <p class="mt-4">Example POST request:</p>
     <pre class="max-w-full overflow-x-auto bg-blue-100 p-2">
 <code class="">$ MAILCOACH_TOKEN="your API token"
-$ curl -x POST {{ action(\Spatie\Mailcoach\Http\Api\Controllers\Automations\TriggerAutomationController::class, [$this->automation]) }} \
+$ curl -x POST {{ action('\\' . \Spatie\Mailcoach\Http\Api\Controllers\Automations\TriggerAutomationController::class, [$this->automation]) }} \
     -H "Authorization: Bearer $MAILCOACH_TOKEN" \
     -H 'Accept: application/json' \
     -H 'Content-Type: application/json'
