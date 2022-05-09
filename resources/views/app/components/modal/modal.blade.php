f6f1369b54567f2afc4da8eeef98aa12b20c2e44
Rias <hey@rias.be>
2022-05-09 11:13:50 +0200

    Add way to open modal from blade

--- resources/views/app/components/modal/modal.blade.php
+++ resources/views/app/components/modal/modal.blade.php
@@ -4,10 +4,13 @@
     'title' => null,
     'confirmText' => __('mailcoach - Confirm'),
     'cancelText' =>  __('mailcoach - Cancel'),
+    'open' => false,
 ])
 @push('modals')
     <!-- {{ $name }} Modal -->
     <div
+        x-data
+        @if ($open) x-init="() => $store.modals.open('{{ $name }}')" @endif
         x-show="$store.modals.isOpen('{{ $name }}')"
         style="display: none"
         x-on:keydown.escape.prevent.stop="$store.modals.close('{{ $name }}')"
