fdb6f5b84646c709df7a45b2cba7c1acd7775cad
riasvdv <riasvdv@users.noreply.github.com>
2022-05-12 14:08:02 +0000

    Fix styling

--- src/Http/App/Livewire/Automations/AutomationMailDelivery.php
+++ src/Http/App/Livewire/Automations/AutomationMailDelivery.php
@@ -4,8 +4,6 @@ namespace Spatie\Mailcoach\Http\App\Livewire\Automations;
 
 use Illuminate\Contracts\View\View;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Support\Collection;
-use Illuminate\Validation\Rule;
 use Livewire\Component;
 use Spatie\Mailcoach\Domain\Automation\Models\AutomationMail;
 use Spatie\Mailcoach\Domain\Shared\Traits\UsesMailcoachModels;
@@ -29,7 +27,7 @@ class AutomationMailDelivery extends Component
     public function render(): View
     {
         return view('mailcoach::app.automations.mails.delivery', [
-            'links' => $this->mail->htmlLinks()
+            'links' => $this->mail->htmlLinks(),
         ])->layout('mailcoach::app.automations.mails.layouts.automationMail', [
             'title' => __('mailcoach - Delivery'),
             'mail' => $this->mail,
