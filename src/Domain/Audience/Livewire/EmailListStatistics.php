753e15b3f8fe2adfd0baa4d37461bf45ab257138
riasvdv <riasvdv@users.noreply.github.com>
2022-05-12 09:26:17 +0000

    Fix styling

--- src/Domain/Audience/Livewire/EmailListStatistics.php
+++ src/Domain/Audience/Livewire/EmailListStatistics.php
@@ -2,19 +2,8 @@
 
 namespace Spatie\Mailcoach\Domain\Audience\Livewire;
 
-use Carbon\Carbon;
-use Carbon\CarbonInterface;
-use Carbon\CarbonPeriod;
-use Illuminate\Contracts\View\View;
-use Illuminate\Database\MySqlConnection;
-use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\Date;
-use Illuminate\Support\Facades\DB;
 use Livewire\Component;
-use Spatie\Mailcoach\Domain\Audience\Models\EmailList;
-use Spatie\Mailcoach\Domain\Shared\Traits\UsesMailcoachModels;
 
 class EmailListStatistics extends Component
 {
-
 }
