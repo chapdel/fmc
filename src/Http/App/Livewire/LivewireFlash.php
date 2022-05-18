191075dd67e4a2f324e769b6facb490ac45f71f2
Freek Van der Herten <freek@spatie.be>
2022-05-18 19:54:11 +0200

    wip

--- src/Http/App/Livewire/LivewireFlash.php
+++ src/Http/App/Livewire/LivewireFlash.php
@@ -4,6 +4,27 @@ namespace Spatie\Mailcoach\Http\App\Livewire;
 
 trait LivewireFlash
 {
+    public function flashSuccess(string $message): self
+    {
+        $this->flash($message, 'success');
+
+        return $this;
+    }
+
+    public function flashWarning(string $message): self
+    {
+        $this->flash($message, 'warning');
+
+        return $this;
+    }
+
+    public function flashError(string $message): self
+    {
+        $this->flash($message, 'error');
+
+        return $this;
+    }
+
     public function flash(string $message, string $level = 'success')
     {
         $this->dispatchBrowserEvent('notify', [
