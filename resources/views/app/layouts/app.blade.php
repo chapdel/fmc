3d4373cca9ee5ee5dc75da0838a86416bbf0038e
Rias <hey@rias.be>
2022-05-18 19:45:13 +0200

    Fix breadcrumbs

--- resources/views/app/layouts/app.blade.php
+++ resources/views/app/layouts/app.blade.php
@@ -68,14 +68,16 @@
 
                     <div class="h-12 flex items-center gap-x-4 px-4 text-gray-700 text-sm">
                         <a class="font-semibold" href="{{ route(config('mailcoach.redirect_home')) }}"><i class="fa fa-home"></i></a>
-                        <span>&gt;</span>
                         @foreach (app(\Spatie\Mailcoach\MainNavigation::class)->breadcrumbs() as $breadcrumb)
+                            @if ($loop->first)
+                                <span>&gt;</span>
+                            @endif
                             <a class="font-semibold" href="{{ $breadcrumb['url'] }}">{{ $breadcrumb['title'] }}</a>
                             @if (! $loop->last)
                                 <span>&gt;</span>
                             @endif
                         @endforeach
-                        @if (isset($title) && $title !== $breadcrumb['title'])
+                        @if (isset($title) && $title !== ($breadcrumb['title'] ?? ''))
                             <span>&gt;</span>
                             <span>
                                 @if (($originTitle ?? '') && $originTitle !== $title)
