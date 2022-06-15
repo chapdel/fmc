e1d77c915766e63341c903d6a3148d5cf1570db3
Rias <hey@rias.be>
2022-06-15 17:20:48 +0200

    Update mailer settings on list

--- resources/views/app/emailLists/settings/mailers.blade.php
+++ resources/views/app/emailLists/settings/mailers.blade.php
@@ -8,52 +8,31 @@
     @if(count(config('mail.mailers')) > 1)
         <x-mailcoach::fieldset :legend="__('mailcoach - Mailers')">
 
-        <div class="form-field">
-            <label class="label">{{ __('mailcoach - Campaign mailer') }}</label>
-            <div class="radio-group">
-                @foreach (config('mail.mailers') as $key => $settings)
-                    <x-mailcoach::radio-field
-                        name="campaign_mailer"
-                        :option-value="$key"
-                        wire:model="emailList.campaign_mailer"
-                        :label="$key"
-                    />
-                @endforeach
-            </div>
-        </div>
+        <x-mailcoach::select-field
+            name="campaign_mailer"
+            :options="array_combine(array_keys(config('mail.mailers')), array_keys(config('mail.mailers')))"
+            wire:model="emailList.campaign_mailer"
+            :label="__('mailcoach - Campaign mailer')"
+        />
         <x-mailcoach::help>{{ __('mailcoach - The mailer used for sending campaigns.') }}</x-mailcoach::help>
 
 
-            <div class="form-field">
-                <label class="label">{{ __('mailcoach - Automations mailer') }}</label>
-                <div class="radio-group">
-                    @foreach (config('mail.mailers') as $key => $settings)
-                        <x-mailcoach::radio-field
-                            name="automation_mailer"
-                            :option-value="$key"
-                            wire:model="emailList.automation_mailer"
-                            :label="$key"
-                        />
-                    @endforeach
-                </div>
-            </div>
-            <x-mailcoach::help>{{ __('mailcoach - The mailer used for sending automations.') }}</x-mailcoach::help>
+        <x-mailcoach::select-field
+            name="automation_mailer"
+            :options="array_combine(array_keys(config('mail.mailers')), array_keys(config('mail.mailers')))"
+            wire:model="emailList.automation_mailer"
+            :label="__('mailcoach - Automation mailer')"
+        />
+        <x-mailcoach::help>{{ __('mailcoach - The mailer used for sending automations.') }}</x-mailcoach::help>
 
 
-            <div class="form-field">
-            <label class="label">{{ __('mailcoach - Transactional mailer') }}</label>
-            <div class="radio-group">
-                @foreach (config('mail.mailers') as $key => $settings)
-                    <x-mailcoach::radio-field
-                        name="transactional_mailer"
-                        :option-value="$key"
-                        wire:model="emailList.transactional_mailer"
-                        :label="$key"
-                    />
-                @endforeach
-            </div>
-        </div>
-        <x-mailcoach::help>{{ __('mailcoach - The mailer used for sending confirmation and welcome mails.') }}</x-mailcoach::help>
+        <x-mailcoach::select-field
+            name="transactional_mailer"
+            :options="array_combine(array_keys(config('mail.mailers')), array_keys(config('mail.mailers')))"
+            wire:model="emailList.transactional_mailer"
+            :label="__('mailcoach - Transactional mailer')"
+        />
+        <x-mailcoach::help>{{ __('mailcoach - The mailer used for sending transactional mails.') }}</x-mailcoach::help>
 
         </x-mailcoach::fieldset>
     @endif
