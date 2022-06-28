6b330ad5b372926b3bac8f63f3a7c6c6009ec91f
Rias <hey@rias.be>
2022-06-28 19:07:21 +0200

    No need to check on type anymore

--- resources/views/app/partials/tag.blade.php
+++ resources/views/app/partials/tag.blade.php
@@ -1,12 +1,3 @@
-@if ($tag->type === \Spatie\Mailcoach\Domain\Campaign\Enums\TagType::Mailcoach)
-    <span class="tag">
-        <span class="inline-block w-8 -ml-2 py-1 pl-2 mr-1 rounded-full bg-blue-400">
-            @include('mailcoach::app.layouts.partials.logoSvg')
-        </span>
-        {{ str_replace('mc::', '', $tag->name) }}
-    </span>
-@else
-    <a href="{{ route('mailcoach.emailLists.tags.edit', [$emailList, $tag]) }}" class="{{ $highlight ?? false ? 'tag' : 'tag-neutral' }}">
-        {{ $tag->name }}
-    </a>
-@endif
+<a href="{{ route('mailcoach.emailLists.tags.edit', [$emailList, $tag]) }}" class="{{ $highlight ?? false ? 'tag' : 'tag-neutral' }}">
+    {{ $tag->name }}
+</a>
