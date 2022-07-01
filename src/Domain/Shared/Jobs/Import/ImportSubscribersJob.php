2e721d2b082c9d3dcbdbdf4d3c058cf7a7e5f94c
Rias <hey@rias.be>
2022-07-01 10:08:57 +0200

    Fix arr except

--- src/Domain/Shared/Jobs/Import/ImportSubscribersJob.php
+++ src/Domain/Shared/Jobs/Import/ImportSubscribersJob.php
@@ -39,9 +39,9 @@ class ImportSubscribersJob extends ImportJob
 
                 $subscribers->whereNotIn('uuid', $existingSubscriberUuids)->each(function (array $subscriber) use ($emailLists) {
                     $subscriber['email_list_id'] = $emailLists[$subscriber['email_list_uuid']];
-                    $columns = Arr::except(Schema::getColumnListing(self::getSubscriberTableName()), ['id']);
+                    $columns = Schema::getColumnListing(self::getSubscriberTableName());
 
-                    dispatch(new ImportSubscriberJob(array_filter(Arr::only($subscriber, $columns))));
+                    dispatch(new ImportSubscriberJob(array_filter(Arr::except(Arr::only($subscriber, $columns), ['id']))));
                 });
 
                 $index += $chunkCount;
