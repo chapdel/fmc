b47c04176c38338cecebcde59687794d94158284
Rias <hey@rias.be>
2022-07-04 14:42:06 +0200

    Fix SubscriberImport model

--- src/Http/App/Controllers/SubscriberImports/DownloadSubscriberImportAttachmentController.php
+++ src/Http/App/Controllers/SubscriberImports/DownloadSubscriberImportAttachmentController.php
@@ -3,12 +3,17 @@
 namespace Spatie\Mailcoach\Http\App\Controllers\SubscriberImports;
 
 use Spatie\Mailcoach\Domain\Audience\Models\SubscriberImport;
+use Spatie\Mailcoach\Domain\Shared\Traits\UsesMailcoachModels;
 
 class DownloadSubscriberImportAttachmentController
 {
+    use UsesMailcoachModels;
+
     public function __invoke(SubscriberImport $subscriberImport, string $collection)
     {
-        abort_unless(! ! $subscriberImport->getMediaCollection($collection), 403);
+        abort_unless((bool)$subscriberImport->getMediaCollection($collection), 403);
+
+        $subscriberImport = self::getSubscriberImportClass()::find($subscriberImport->id);
 
         return $subscriberImport->getFirstMedia($collection);
     }
