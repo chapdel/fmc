a202e274c7a26c88ba249570934c6d2b14c18342
Rias <hey@rias.be>
2022-07-06 10:47:58 +0200

    wip

---
+++ resources/js/components/modals.js
@@ -0,0 +1,24 @@
+import Alpine from 'alpinejs';
+
+document.addEventListener('alpine:init', () => {
+    Alpine.store('modals', {
+        openModals: [],
+        onConfirm: null,
+        init() {
+            if (window.location.hash) {
+                this.openModals.push(window.location.hash.replace('#', ''));
+            }
+        },
+        isOpen(id) {
+            return this.openModals.includes(id);
+        },
+        open(id) {
+            this.openModals.push(id);
+            window.location.hash = id;
+        },
+        close(id) {
+            this.openModals = this.openModals.filter(modal => modal !== id);
+            history.pushState('', document.title, window.location.pathname + window.location.search);
+        },
+    });
+});
