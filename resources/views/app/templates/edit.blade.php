805c1000afaa5da8303927ec974e3b6702e1190b
Rias <hey@rias.be>
2022-07-12 09:58:23 +0200

    Add small guide to templates

--- resources/views/app/templates/edit.blade.php
+++ resources/views/app/templates/edit.blade.php
@@ -1,6 +1,11 @@
 <div>
+    <x-mailcoach::help>
+        <p>{{ __('mailcoach - A template is a reusable layout that can be used as a starting point for your campaigns, automation emails or transactional mails.') }}</p>
+        <span>{!! __('mailcoach - Create slots in your template by adding the name in triple brackets, for example: <code>[[[content]]]</code>. You can add as many slots as you like.') !!}</span>
+    </x-mailcoach::help>
+
     <form
-        class="form-grid"
+        class="form-grid mt-6"
         wire:submit.prevent="save"
         @keydown.prevent.window.cmd.s="$wire.call('save')"
         @keydown.prevent.window.ctrl.s="$wire.call('save')"
