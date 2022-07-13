726aac77a7eb30d0eb8b0f53211a8187e9745e22
Rias <hey@rias.be>
2022-07-13 09:31:30 +0200

    Fix root on transactional templates

--- resources/views/app/transactionalMails/templates/edit.blade.php
+++ resources/views/app/transactionalMails/templates/edit.blade.php
@@ -1,25 +1,30 @@
-<form
-class="card-grid"
-method="POST"
-wire:submit.prevent="save"
-@keydown.prevent.window.cmd.s="$wire.call('save')"
-@keydown.prevent.window.ctrl.s="$wire.call('save')"
->
-<x-mailcoach::fieldset card :legend="__('mailcoach - Recipients')">
-    <x-mailcoach::info>
-        {{ __('mailcoach - These recipients will be merged with the ones when the mail is sent. You can specify multiple recipients comma separated.') }}
-    </x-mailcoach::info>
-    <x-mailcoach::text-field placeholder="john@example.com, jane@example.com" :label="__('mailcoach - To')" name="template.to" wire:model.lazy="template.to"/>
-    <x-mailcoach::text-field placeholder="john@example.com, jane@example.com" :label="__('mailcoach - Cc')" name="template.cc" wire:model.lazy="template.cc"/>
-    <x-mailcoach::text-field placeholder="john@example.com, jane@example.com" :label="__('mailcoach - Bcc')" name="template.bcc" wire:model.lazy="template.bcc"/>
-</x-mailcoach::fieldset>
+<div>
+    <form
+        class="card-grid"
+        method="POST"
+        wire:submit.prevent="save"
+        @keydown.prevent.window.cmd.s="$wire.call('save')"
+        @keydown.prevent.window.ctrl.s="$wire.call('save')"
+    >
+        <x-mailcoach::fieldset card :legend="__('mailcoach - Recipients')">
+            <x-mailcoach::info>
+                {{ __('mailcoach - These recipients will be merged with the ones when the mail is sent. You can specify multiple recipients comma separated.') }}
+            </x-mailcoach::info>
+            <x-mailcoach::text-field placeholder="john@example.com, jane@example.com" :label="__('mailcoach - To')"
+                                     name="template.to" wire:model.lazy="template.to"/>
+            <x-mailcoach::text-field placeholder="john@example.com, jane@example.com" :label="__('mailcoach - Cc')"
+                                     name="template.cc" wire:model.lazy="template.cc"/>
+            <x-mailcoach::text-field placeholder="john@example.com, jane@example.com" :label="__('mailcoach - Bcc')"
+                                     name="template.bcc" wire:model.lazy="template.bcc"/>
+        </x-mailcoach::fieldset>
 
-<x-mailcoach::fieldset card :legend="__('mailcoach - Email')">
-        
-<x-mailcoach::text-field :label="__('mailcoach - Subject')" name="template.subject" wire:model.lazy="template.subject" required/>
+        <x-mailcoach::fieldset card :legend="__('mailcoach - Email')">
 
-        @livewire(\Livewire\Livewire::getAlias(config('mailcoach.template_editor')), ['model' => $template])
-    </x-mailcoach::fieldset>
+            <x-mailcoach::text-field :label="__('mailcoach - Subject')" name="template.subject"
+                                     wire:model.lazy="template.subject" required/>
+
+            @livewire(\Livewire\Livewire::getAlias(config('mailcoach.template_editor')), ['model' => $template])
+        </x-mailcoach::fieldset>
     </form>
 
     <x-mailcoach::preview-modal :title="__('mailcoach - Preview') . ' - ' . $template->subject" :html="$template->body"/>
@@ -31,3 +36,4 @@ wire:submit.prevent="save"
     @endif
 
     <x-mailcoach::transactional-mail-template-replacer-help-texts :template="$template"/>
+</div>
