ea51604c7e2467631713c1c5da8b9437e06588c7
Willem Van Bockstal <willem@spatie.be>
2022-07-20 16:19:52 +0200

    Team switcher changes

--- resources/views/app/components/navigation/root.blade.php
+++ resources/views/app/components/navigation/root.blade.php
@@ -2,7 +2,7 @@
     'main' => false,
 ])
 <div class="md:sticky md:top-14">
-    <ul class="flex flex-col gap-2 md:gap-3 p-6 md:px-10 md:py-8 bg-gradient-to-b from-indigo-600/5 to-indigo-600/10 rounded-b-md md:rounded-md bg-clip-padding border border-indigo-700/10">
+    <ul class="flex flex-col gap-2 md:gap-3 p-6 md:px-8 md:py-8 bg-gradient-to-b from-indigo-600/5 to-indigo-600/10 rounded-b-md md:rounded-md bg-clip-padding border border-indigo-700/10">
         {{ $slot }}
     </ul>
 </div>
