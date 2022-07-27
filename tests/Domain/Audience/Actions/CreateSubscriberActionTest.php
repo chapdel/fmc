80ec7f45a521a932d21501c14800d41a25ee79f8
Rias <hey@rias.be>
2022-07-27 16:05:04 +0200

    Imports should not dispatch event

--- tests/Domain/Audience/Actions/CreateSubscriberActionTest.php
+++ tests/Domain/Audience/Actions/CreateSubscriberActionTest.php
@@ -1,5 +1,8 @@
 <?php
 
+use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Str;
+use Spatie\Mailcoach\Domain\Audience\Events\SubscribedEvent;
 use Spatie\Mailcoach\Domain\Audience\Models\EmailList;
 use Spatie\Mailcoach\Domain\Audience\Models\Subscriber;
 use Spatie\Mailcoach\Domain\Audience\Support\PendingSubscriber;
@@ -31,4 +34,20 @@ it('does not override the subscribed_at', function () {
     $existingSubscriber->refresh();
 
     expect($existingSubscriber->subscribed_at->startOfSecond())->toEqual(now()->subDays(1)->startOfSecond());
+});
+
+it('does not dispatch an event when importing', function () {
+    Event::fake(SubscribedEvent::class);
+
+    $emailList = EmailList::factory()->create();
+
+    self::getSubscriberClass()::createWithEmail('john@doe.com', [
+        'imported_via_import_uuid' => Str::uuid()->toString(),
+    ])
+        ->skipConfirmation()
+        ->tags([])
+        ->replaceTags(true)
+        ->subscribeTo($emailList);
+
+    Event::assertNotDispatched(SubscribedEvent::class);
 });
