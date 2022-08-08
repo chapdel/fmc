632ef15937f48378c70ec4e8398d883c09e6e185
Rias <hey@rias.be>
2022-08-08 17:03:23 +0200

    Change commands to jobs

--- src/Domain/Automation/Commands/SendAutomationMailsCommand.php
+++ src/Domain/Automation/Commands/SendAutomationMailsCommand.php
@@ -3,29 +3,21 @@
 namespace Spatie\Mailcoach\Domain\Automation\Commands;
 
 use Illuminate\Console\Command;
-use Spatie\Mailcoach\Domain\Automation\Actions\SendAutomationMailsAction;
-use Spatie\Mailcoach\Domain\Automation\Exceptions\SendAutomationMailsTimeLimitApproaching;
-use Spatie\Mailcoach\Mailcoach;
+use Spatie\Mailcoach\Domain\Automation\Jobs\SendAutomationMailsJob;
 
 class SendAutomationMailsCommand extends Command
 {
-    public $signature = 'mailcoach:send-automation-mails';
+    public $signature = 'mailcoach:send-automation-mails {--sync=false}';
 
     public $description = 'Send pending automation mails.';
 
     public function handle()
     {
-        /** @var \Spatie\Mailcoach\Domain\Automation\Actions\SendAutomationMailsAction $sendAutomationMailsAction */
-        $sendAutomationMailsAction = Mailcoach::getAutomationActionClass('send_automation_mails_action', SendAutomationMailsAction::class);
-
-        $maxRuntimeInSeconds = max(60, config('mailcoach.automation.send_automation_mails_maximum_job_runtime_in_seconds'));
-
-        $stopExecutingAt = now()->addSeconds($maxRuntimeInSeconds);
-
-        try {
-            $sendAutomationMailsAction->execute($stopExecutingAt);
-        } catch (SendAutomationMailsTimeLimitApproaching) {
+        if ($this->option('sync')) {
+            dispatch_sync(new SendAutomationMailsJob());
             return;
         }
+
+        dispatch(new SendAutomationMailsJob());
     }
 }
