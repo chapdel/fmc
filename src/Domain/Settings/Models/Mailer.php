99e48c2f6b7d3c7d2102daa2cf888f67c7000e16
Rias <hey@rias.be>
2022-08-11 15:52:16 +0200

    Register correct secret

--- src/Domain/Settings/Models/Mailer.php
+++ src/Domain/Settings/Models/Mailer.php
@@ -75,7 +75,7 @@ class Mailer extends Model
                     'mails_per_timespan' => $this->get('mails_per_timespan'),
                 ]);
 
-                config()->set('mailcoach.sendgrid.signing_secret', $this->get('signing_secret'));
+                config()->set('mailcoach.sendgrid_feedback.signing_secret', $this->get('signing_secret'));
 
                 break;
             case MailerTransport::Smtp:
