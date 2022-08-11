3589c1a2fd327b8d5473f8a84ab30cb9ec8e4439
Freek Van der Herten <freek@spatie.be>
2022-08-11 12:04:57 +0200

    accept mailer for transactional mails

---
+++ src/Domain/Settings/Rules/MailerConfigNameRule.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Spatie\Mailcoach\Domain\Settings\Rules;
+
+use Spatie\Mailcoach\Domain\Settings\Models\Mailer;
+
+class MailerConfigNameRule
+{
+    public function passes($attribute, $value)
+    {
+        $mailerConfigNames = Mailer::all()->map(fn(Mailer $mailer) => $mailer->configName());
+
+        return in_array($value, $mailerConfigNames);
+    }
+
+    public function message()
+    {
+        $mailerConfigNames = Mailer::all()
+            ->map(fn(Mailer $mailer) => "`{$mailer->configName()}`")
+            ->join(', ', ' and ');
+
+
+        return "You must pass a valid mailer key. Valid keys are: {$mailerConfigNames}.";
+    }
+}
