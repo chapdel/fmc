8ead6ec8143b319cbe8e578f15a59ecbbf14ff0d
Rias <hey@rias.be>
2022-08-11 13:50:20 +0200

    Add config key name to database

--- tests/Domain/Settings/MailerTest.php
+++ tests/Domain/Settings/MailerTest.php
@@ -17,3 +17,12 @@ it('can get and set configuration values', function () {
     expect($mailer->get('b'))->toBe('second value');
     expect($mailer->get('c'))->toBe('third value');
 });
+
+it('sets config key name when creating and can find by config key name', function () {
+    $mailer = Mailer::factory()->create(['name' => 'Some name']);
+
+    expect($mailer->config_key_name)->toBe('mailcoach-some-name');
+    expect($mailer->configName())->toBe('mailcoach-some-name');
+
+    expect(Mailer::findByConfigKeyName('mailcoach-some-name')->is($mailer))->toBeTrue();
+});
