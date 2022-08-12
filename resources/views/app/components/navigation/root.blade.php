6efaa7ed7fbed484bb67a15b78ac0242b0ba437e
Willem Van Bockstal <willem@spatie.be>
2022-08-12 11:26:20 +0200

    Closes #40

--- resources/views/app/components/navigation/root.blade.php
+++ resources/views/app/components/navigation/root.blade.php
@@ -1,8 +1,11 @@
 @props([
-    'main' => false,
+    'title' => '',
 ])
 <div class="md:sticky md:top-14">
-    <ul class="flex flex-col gap-2 md:gap-3 p-6 md:px-8 md:py-8 bg-gradient-to-b from-indigo-600/5 to-indigo-600/10 rounded-b-md md:rounded-md bg-clip-padding border border-indigo-700/10">
-        {{ $slot }}
-    </ul>
+    <div class="p-6 md:px-8 md:py-8 bg-gradient-to-b from-indigo-600/5 to-indigo-600/10 rounded-b-md md:rounded-md bg-clip-padding border border-indigo-700/10">
+        <h2 class="mb-6 font-extrabold text-xs uppercase tracking-wider truncate">{{ $title ?? '' }}</h2>
+        <ul class="flex flex-col gap-2 md:gap-3">
+            {{ $slot }}
+        </ul>
+    </div>
 </div>
