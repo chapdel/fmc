de28580a33e7437e9ce0a78b21e6bbd2a300b60f
Rias <hey@rias.be>
2022-08-29 10:12:24 +0200

    Remove title from default snippet

--- src/Domain/Campaign/Actions/Templates/CreateTemplateAction.php
+++ src/Domain/Campaign/Actions/Templates/CreateTemplateAction.php
@@ -20,8 +20,6 @@ class CreateTemplateAction
             <head>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-
-                <title>{$attributes['name']}</title>
             </head>
 
             <body>
