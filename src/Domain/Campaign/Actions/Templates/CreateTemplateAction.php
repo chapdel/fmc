b4b7e9750ccb202c111e6748a439504e4842ac69
riasvdv <riasvdv@users.noreply.github.com>
2022-08-29 08:13:11 +0000

    Fix styling

--- src/Domain/Campaign/Actions/Templates/CreateTemplateAction.php
+++ src/Domain/Campaign/Actions/Templates/CreateTemplateAction.php
@@ -13,7 +13,7 @@ class CreateTemplateAction
     {
         return self::getTemplateClass()::create([
             'name' => $attributes['name'],
-            'html' => $attributes['html'] ?? <<<html
+            'html' => $attributes['html'] ?? <<<'html'
             <!-- You can customise this template however you want -->
             <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
             <html xmlns="http://www.w3.org/1999/xhtml">
