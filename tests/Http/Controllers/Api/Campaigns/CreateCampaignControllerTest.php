b15938e334e90a6b0cc4d9da0da1894086ef2d98
Freek Van der Herten <freek@spatie.be>
2022-09-12 16:00:25 +0200

    add creation test

--- tests/Http/Controllers/Api/Campaigns/CreateCampaignControllerTest.php
+++ tests/Http/Controllers/Api/Campaigns/CreateCampaignControllerTest.php
@@ -5,10 +5,12 @@ use Spatie\Mailcoach\Domain\Audience\Models\EmailList;
 use Spatie\Mailcoach\Domain\Audience\Models\TagSegment;
 use Spatie\Mailcoach\Domain\Campaign\Enums\CampaignStatus;
 use Spatie\Mailcoach\Domain\Campaign\Models\Campaign;
+use Spatie\Mailcoach\Domain\Campaign\Models\Template;
 use Spatie\Mailcoach\Domain\Campaign\Policies\CampaignPolicy;
 use Spatie\Mailcoach\Http\Api\Controllers\Campaigns\CampaignsController;
 use Spatie\Mailcoach\Tests\Http\Controllers\Api\Concerns\RespondsToApiRequests;
 use Spatie\Mailcoach\Tests\TestClasses\CustomCampaignDenyAllPolicy;
+use Spatie\MailcoachMarkdownEditor\Editor as MarkdownEditor;
 
 uses(RespondsToApiRequests::class);
 
@@ -52,6 +54,35 @@ test('access is denied by custom authorization policy', function () {
         ->withExceptionHandling()
         ->postJson(action([CampaignsController::class, 'store']), test()->postAttributes)
         ->assertForbidden();
+});
+
+it('can accept the values for a template when creating a campaign and using a markdown editor', function() {
+    config()->set('mailcoach.content_editor', MarkdownEditor::class);
+
+    $template = Template::create([
+        'html' => $this->stub('TemplateHtml/default.html'),
+        'contains_placeholders' => true,
+        'name' => 'Default',
+    ]);
+
+    $attributes = [
+        'name' => 'name',
+        'email_list_uuid' => EmailList::factory()->create()->uuid,
+        'fields' => [
+            'title' => 'This is my title',
+            'content' => '# This is some markdown'
+        ],
+        'template_uuid' => $template->uuid,
+    ];
+
+    $this
+        ->postJson(action([CampaignsController::class, 'store']), $attributes)
+        ->assertSuccessful();
+
+    $campaign = Campaign::first();
+
+    $this->assertMatchesSnapshot($campaign->refresh()->structured_html);
+    $this->assertMatchesSnapshot($campaign->refresh()->html);
 });
 
 // Helpers
