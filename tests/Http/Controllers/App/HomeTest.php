823baa0eef7cc0867d444e3cac8988a18274bd71
freekmurze <freekmurze@users.noreply.github.com>
2022-09-21 12:48:30 +0000

    Fix styling

--- tests/Http/Controllers/App/HomeTest.php
+++ tests/Http/Controllers/App/HomeTest.php
@@ -1,5 +1,5 @@
 <?php
 
-it('the home url will redirect to the mailcoach dashboard page when not authenticated', function() {
+it('the home url will redirect to the mailcoach dashboard page when not authenticated', function () {
     $this->get('/mailcoach')->assertRedirect(route('mailcoach.dashboard'));
 });
