f268c0827abb23d69f150669ce5b7ea2be7b85a1
Rias <hey@rias.be>
2022-09-22 13:55:52 +0200

    Show webview html if available in read only content

--- resources/views/app/campaigns/contentReadOnly.blade.php
+++ resources/views/app/campaigns/contentReadOnly.blade.php
@@ -1,7 +1,7 @@
 <x-mailcoach::layout-campaign :title="__('mailcoach - Content')" :campaign="$campaign">
     <div class="card-grid">
         <x-mailcoach::card>
-            <x-mailcoach::html-field :label="__('mailcoach - Body (HTML)')" name="html" :value="$campaign->html" :disabled="! $campaign->isEditable()" />
+            <x-mailcoach::html-field :label="__('mailcoach - Body (HTML)')" name="html" :value="$campaign->webview_html ?? $campaign->html" :disabled="! $campaign->isEditable()" />
         </x-mailcoach::card>
 
         <x-mailcoach::card>
