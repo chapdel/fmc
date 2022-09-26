78ad67be731b2005f34e23a25ef1e4367d88d51e
Rias <hey@rias.be>
2022-09-26 11:05:07 +0200

    Add a link to website on the email list index

--- resources/views/app/emailLists/partials/row.blade.php
+++ resources/views/app/emailLists/partials/row.blade.php
@@ -3,6 +3,11 @@
         <a class="break-words" href="{{ route('mailcoach.emailLists.summary', $row) }}">
             {{ $row->name }}
         </a>
+        @if ($row->has_website)
+            <a class="link text-sm ml-2" title="{{ __('mailcoach - Website') }}" href="{{ $row->websiteUrl() }}" target="_blank">
+                <i class="fas fa-external-link"></i>
+            </a>
+        @endif
     </td>
     <td class="td-numeric">{{ number_format($row->active_subscribers_count) }}</td>
     <td class="td-numeric hidden | xl:table-cell">
