58dd2585b6bfa5c089d0575d16f0a71802c886f4
riasvdv <riasvdv@users.noreply.github.com>
2022-09-27 09:52:45 +0000

    Fix styling

--- src/helpers.php
+++ src/helpers.php
@@ -2,7 +2,7 @@
 
 function __mc(string $key, array $replace = [], ?string $locale = null): string
 {
-    $result = __('mailcoach::mailcoach.' . $key, $replace, $locale);
+    $result = __('mailcoach::mailcoach.'.$key, $replace, $locale);
 
     return str_replace('mailcoach::mailcoach.', '', $result);
 }
