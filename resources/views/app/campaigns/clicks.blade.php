cac0e6588bef79f9221c3ac50fd07779291be41d
Rias <hey@rias.be>
2022-09-27 14:22:42 +0200

    Only display tags if it's enabled

--- resources/views/app/campaigns/clicks.blade.php
+++ resources/views/app/campaigns/clicks.blade.php
@@ -4,12 +4,12 @@
             name="clicks"
             :rows="$links ?? null"
             :totalRowsCount="$totalLinksCount ?? null"
-            :columns="[
+            :columns="array_filter([
                 ['attribute' => 'link', 'label' => __mc('Link')],
-                ['label' => __mc('Tag')],
+                $campaign->add_subscriber_link_tags ? ['label' => __mc('Tag')] : null,
                 ['attribute' => '-unique_click_count', 'label' => __mc('Unique Clicks'), 'class' => 'w-32 th-numeric hidden | xl:table-cell'],
                 ['attribute' => '-click_count', 'label' => __mc('Clicks'), 'class' => 'w-32 th-numeric'],
-            ]"
+            ])"
             rowPartial="mailcoach::app.campaigns.partials.clickRow"
             :rowData="[
                 'campaign' => $campaign,
