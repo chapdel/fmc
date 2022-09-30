88ea4c814bd97307938f0f3e38137fa8545733b6
Rias <hey@rias.be>
2022-09-30 16:26:08 +0200

    Fix charts

--- resources/js/components/charts/dashboardChart.js
+++ resources/js/components/charts/dashboardChart.js
@@ -122,6 +122,7 @@ document.addEventListener('alpine:init', () => {
                     },
                     scales: {
                         y: {
+                            display: false,
                             ticks: {
                                 color: 'rgba(100, 116, 139, 1)',
                                 precision: 0,
@@ -135,7 +136,7 @@ document.addEventListener('alpine:init', () => {
                                 color: 'rgba(100, 116, 139, 1)',
                                 precision: 0,
                             },
-                            position: 'right',
+                            position: 'left',
                             beginAtZero: false,
                             grid: {
                                 display: false,
