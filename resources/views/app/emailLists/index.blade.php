455a875471fcb2c036579c02fd882be0f82b59a6
Freek Van der Herten <freek@spatie.be>
2022-10-01 14:47:22 +0200

    fix list view

--- resources/views/app/emailLists/index.blade.php
+++ resources/views/app/emailLists/index.blade.php
@@ -1,12 +1,13 @@
 <x-mailcoach::data-table
-    :query="Post::query()"
+    name="list"
+    :modelClass="\Spatie\Mailcoach\Mailcoach::getEmailListClass()"
+    :rows="$emailLists ?? null"
+    :totalRowsCount="$totalEmailListsCount ?? null"
     :columns="[
         ['attribute' => 'name', 'label' => __mc('Name')],
         ['attribute' => '-active_subscribers_count', 'label' => __mc('Active'), 'class' => 'w-32 th-numeric'],
         ['attribute' => '-created_at', 'label' => __mc('Created'), 'class' => 'w-48 th-numeric hidden | xl:table-cell'],
         ['class' => 'w-12'],
-        Column::new($name)->label('sdmlfskjdfm')->initialSort()->class('w-48')->notSortable()->notSearchable(),
-        Column::new()->class('w-12'),
     ]"
     rowPartial="mailcoach::app.emailLists.partials.row"
     :emptyText="
@@ -14,19 +15,4 @@
             ? __mc('No email lists found.')
             : __mc('You\'ll need at least one list to gather subscribers.')
     "
->
-    <slot name="row" $row>
-        {{ $row }}
-        @include('smdklqjsmdf')
-    </slot>
-    <slot name="loading">
-
-    </slot>
-    <slot name="noResults">
-
-    </slot>
-    <slot name="header">
-        $columns
-    </slot>
-
-</x-mailcoach::data-table>
+/>
