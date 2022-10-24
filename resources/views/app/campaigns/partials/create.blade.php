1743bb1ce5822aef5c8dc6bf05d4c73bb61daf66
Rias <hey@rias.be>
2022-10-24 16:52:30 +0200

    Remove template dropdown when unlayer

--- resources/views/app/campaigns/partials/create.blade.php
+++ resources/views/app/campaigns/partials/create.blade.php
@@ -21,7 +21,7 @@
         required
     />
 
-    @if(count($templateOptions) > 1)
+    @if(count($templateOptions) > 1 && config('mailcoach.content_editor') !== \Spatie\MailcoachUnlayer\UnlayerEditor::class)
         <x-mailcoach::select-field
             :label="__mc('Template')"
             :options="$templateOptions"
