dd16d0beaa4bd17035460a3f7e608dd662bb445e
Rias <hey@rias.be>
2022-10-25 22:01:18 +0200

    Fix joins in fuzzy search

--- src/Http/App/Queries/Filters/FuzzyFilter.php
+++ src/Http/App/Queries/Filters/FuzzyFilter.php
@@ -29,7 +29,7 @@ class FuzzyFilter implements Filter
                 ->addDirectFields($builder, $values)
                 ->addRelationShipFields($builder, $values);
 
-            $query->getQuery()->joins = $builder->getQuery()->joins;
+            $query->getQuery()->joins = array_merge($query->getQuery()->joins, $builder->getQuery()->joins ?? []);
         });
 
         return $query;
