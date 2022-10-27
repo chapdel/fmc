0f1d84ad91078bd326366ee6044fccc5b0e682c9
riasvdv <riasvdv@users.noreply.github.com>
2022-10-27 18:52:14 +0000

    Fix styling

--- src/Domain/Campaign/Actions/PrepareSubjectAction.php
+++ src/Domain/Campaign/Actions/PrepareSubjectAction.php
@@ -3,14 +3,14 @@
 namespace Spatie\Mailcoach\Domain\Campaign\Actions;
 
 use Spatie\Mailcoach\Domain\Campaign\Models\Campaign;
-use Spatie\Mailcoach\Domain\Campaign\Support\Replacers\CampaignReplacer;
 use Spatie\Mailcoach\Domain\Shared\Actions\ReplacePlaceholdersAction;
 
 class PrepareSubjectAction
 {
     public function __construct(
         protected ReplacePlaceholdersAction $replacePlaceholdersAction
-    ) {}
+    ) {
+    }
 
     public function execute(Campaign $campaign): void
     {
