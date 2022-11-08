fac7f23e663d6cb53f651f196c4951f7ec4c3b31
Ruben Van Assche <rubenvanassche@gmail.com>
2022-11-08 12:15:12 +0100

    Fix test

--- tests/TestClasses/CustomSendAutomationMailAction.php
+++ tests/TestClasses/CustomSendAutomationMailAction.php
@@ -2,13 +2,12 @@
 
 namespace Spatie\Mailcoach\Tests\TestClasses;
 
-use Spatie\Mailcoach\Domain\Audience\Models\Subscriber;
 use Spatie\Mailcoach\Domain\Automation\Models\ActionSubscriber;
 use Spatie\Mailcoach\Domain\Automation\Support\Actions\SendAutomationMailAction;
 
 class CustomSendAutomationMailAction extends SendAutomationMailAction
 {
-    public function run(Subscriber $subscriber, ?ActionSubscriber $actionSubscriber = null): void
+    public function run(ActionSubscriber $subscriber, ?ActionSubscriber $actionSubscriber = null): void
     {
         if (isset($actionSubscriber)) {
             throw new \Exception('ActionSubscriber is set!');
