07391f4e16c746e56ec423e41289ccc899be0d31
Freek Van der Herten <freek@spatie.be>
2022-11-08 13:23:50 +0100

    add template uuid

--- src/Http/Api/Resources/CampaignResource.php
+++ src/Http/Api/Resources/CampaignResource.php
@@ -19,6 +19,7 @@ class CampaignResource extends JsonResource
 
             'email_list_uuid' => $this->emailList->uuid,
             'email_list' => new EmailListResource($this->whenLoaded('emailList')),
+            'template_uuid' => $this->template->uuid,
 
             //'segment' => new SegmentResource($this->whenLoaded('segment')),
 
