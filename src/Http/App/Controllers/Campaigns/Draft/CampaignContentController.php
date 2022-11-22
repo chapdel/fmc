f4efd1e191f5a236ceebdbdbb65f13e5e79bef03
Rias <hey@rias.be>
2022-11-22 09:22:27 +0100

    Use model from config in campaign content controller

--- src/Http/App/Controllers/Campaigns/Draft/CampaignContentController.php
+++ src/Http/App/Controllers/Campaigns/Draft/CampaignContentController.php
@@ -4,18 +4,19 @@ namespace Spatie\Mailcoach\Http\App\Controllers\Campaigns\Draft;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Spatie\Mailcoach\Domain\Campaign\Models\Campaign;
-use Spatie\Mailcoach\Domain\Campaign\Models\Template;
+use Spatie\Mailcoach\Domain\Shared\Traits\UsesMailcoachModels;
 use Spatie\Mailcoach\MainNavigation;
 
 class CampaignContentController
 {
     use AuthorizesRequests;
+    use UsesMailcoachModels;
 
     public function edit(Campaign $campaign)
     {
         $this->authorize('update', $campaign);
 
-        $templateOptions = Template::all()
+        $templateOptions = self::getTemplateClass()::all()
             ->pluck('name', 'id')
             ->toArray();
 
