9e2eae6bf175a39c6bdeb5f50b8cee6e723d556e
Rias <hey@rias.be>
2022-12-20 15:21:14 +0100

    Cache is not worth the headache

--- src/Domain/Shared/Actions/RenderTwigAction.php
+++ src/Domain/Shared/Actions/RenderTwigAction.php
@@ -11,7 +11,6 @@ class RenderTwigAction
     {
         $twig = new Environment(new ArrayLoader(), [
             'autoescape' => 'html',
-            'cache' => sys_get_temp_dir().'/mailcoach-twig',
         ]);
 
         return $twig->createTemplate(urldecode($html))->render($context);
