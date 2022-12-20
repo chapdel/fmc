dcfca74ed52840bb21cbcdf9c7e0f859ebcd524e
Rias <hey@rias.be>
2022-12-20 11:40:33 +0100

    Wire key

--- resources/views/app/automations/mails/delivery.blade.php
+++ resources/views/app/automations/mails/delivery.blade.php
@@ -133,7 +133,7 @@
                 <ul class="grid gap-2">
                     @foreach ($links as $link)
                         <li>
-                            <livewire:mailcoach::link-check :url="$link" />
+                            <livewire:mailcoach::link-check :url="$link" wire:key="{{ $link }}" />
                             @php($tags[] = \Spatie\Mailcoach\Domain\Shared\Support\LinkHasher::hash($mail, $link))
                         </li>
                     @endforeach
