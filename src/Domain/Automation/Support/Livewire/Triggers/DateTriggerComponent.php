f919e40bc88b55a42ac8045e6577e9373f55a560
Rias <hey@rias.be>
2023-01-02 09:22:05 +0100

    Fix display & default of date trigger

--- src/Domain/Automation/Support/Livewire/Triggers/DateTriggerComponent.php
+++ src/Domain/Automation/Support/Livewire/Triggers/DateTriggerComponent.php
@@ -8,6 +8,15 @@ class DateTriggerComponent extends AutomationTriggerComponent
 {
     public ?array $date = null;
 
+    public function mount()
+    {
+        $this->date ??= [
+            'date' => now()->format('Y-m-d'),
+            'hours' => (int) now()->addHour()->format('H'),
+            'minutes' => 0,
+        ];
+    }
+
     public function render()
     {
         return view('mailcoach::app.automations.components.triggers.dateTrigger');
