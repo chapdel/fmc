12d2c12388686900277bd5761c417059bd61145d
Rias <hey@rias.be>
2023-01-03 09:13:10 +0100

    Make CreateSubscriberAction use a lock

--- src/Domain/Audience/Actions/Subscribers/CreateSubscriberAction.php
+++ src/Domain/Audience/Actions/Subscribers/CreateSubscriberAction.php
@@ -2,6 +2,8 @@
 
 namespace Spatie\Mailcoach\Domain\Audience\Actions\Subscribers;
 
+use Illuminate\Contracts\Cache\LockTimeoutException;
+use Illuminate\Support\Facades\Cache;
 use Spatie\Mailcoach\Domain\Audience\Events\SubscribedEvent;
 use Spatie\Mailcoach\Domain\Audience\Models\Subscriber;
 use Spatie\Mailcoach\Domain\Audience\Support\PendingSubscriber;
@@ -14,50 +16,58 @@ class CreateSubscriberAction
 
     public function execute(PendingSubscriber $pendingSubscriber): Subscriber
     {
-        /** @var class-string<Subscriber> $subscriberClass */
-        $subscriberClass = self::getSubscriberClass();
+        $lock = Cache::lock("subscribe-{$pendingSubscriber->email}-{$pendingSubscriber->emailList->id}");
 
-        $subscriber = $subscriberClass::findForEmail($pendingSubscriber->email, $pendingSubscriber->emailList);
+        try {
+            $lock->block(5);
 
-        $wasAlreadySubscribed = $subscriber?->isSubscribed();
+            /** @var class-string<Subscriber> $subscriberClass */
+            $subscriberClass = self::getSubscriberClass();
 
-        if (! $subscriber) {
-            $subscriber = new $subscriberClass([
+            $subscriber = $subscriberClass::findForEmail($pendingSubscriber->email, $pendingSubscriber->emailList);
+
+            $wasAlreadySubscribed = $subscriber?->isSubscribed();
+
+            if (! $subscriber) {
+                $subscriber = new $subscriberClass([
+                    'email' => $pendingSubscriber->email,
+                    'email_list_id' => $pendingSubscriber->emailList->id,
+                ]);
+            }
+
+            $subscriber->fill([
                 'email' => $pendingSubscriber->email,
-                'email_list_id' => $pendingSubscriber->emailList->id,
+                'subscribed_at' => $subscriber->subscribed_at ?? $pendingSubscriber->subscribedAt ?? now(),
+                'unsubscribed_at' => null,
             ]);
+
+            $subscriber->fill($pendingSubscriber->attributes);
+
+            if (! $wasAlreadySubscribed && $pendingSubscriber->emailList->requires_confirmation && $pendingSubscriber->respectDoubleOptIn) {
+                $subscriber->subscribed_at = null;
+            }
+
+            $subscriber->save();
+
+            if ($pendingSubscriber->replaceTags) {
+                $subscriber->syncTags($pendingSubscriber->tags);
+            } elseif ($pendingSubscriber->tags) {
+                $subscriber->addTags($pendingSubscriber->tags);
+            }
+
+            if ($subscriber->isUnconfirmed()) {
+                $sendConfirmSubscriberMailAction = Mailcoach::getAudienceActionClass('send_confirm_subscriber_mail', SendConfirmSubscriberMailAction::class);
+
+                $sendConfirmSubscriberMailAction->execute($subscriber, $pendingSubscriber->redirectAfterSubscribed);
+            }
+
+            if ($subscriber->isSubscribed() && ! $subscriber->imported_via_import_uuid) {
+                event(new SubscribedEvent($subscriber));
+            }
+
+            return $subscriber->refresh();
+        } finally {
+            optional($lock)->release();
         }
-
-        $subscriber->fill([
-            'email' => $pendingSubscriber->email,
-            'subscribed_at' => $subscriber->subscribed_at ?? $pendingSubscriber->subscribedAt ?? now(),
-            'unsubscribed_at' => null,
-        ]);
-
-        $subscriber->fill($pendingSubscriber->attributes);
-
-        if (! $wasAlreadySubscribed && $pendingSubscriber->emailList->requires_confirmation && $pendingSubscriber->respectDoubleOptIn) {
-            $subscriber->subscribed_at = null;
-        }
-
-        $subscriber->save();
-
-        if ($pendingSubscriber->replaceTags) {
-            $subscriber->syncTags($pendingSubscriber->tags);
-        } elseif ($pendingSubscriber->tags) {
-            $subscriber->addTags($pendingSubscriber->tags);
-        }
-
-        if ($subscriber->isUnconfirmed()) {
-            $sendConfirmSubscriberMailAction = Mailcoach::getAudienceActionClass('send_confirm_subscriber_mail', SendConfirmSubscriberMailAction::class);
-
-            $sendConfirmSubscriberMailAction->execute($subscriber, $pendingSubscriber->redirectAfterSubscribed);
-        }
-
-        if ($subscriber->isSubscribed() && ! $subscriber->imported_via_import_uuid) {
-            event(new SubscribedEvent($subscriber));
-        }
-
-        return $subscriber->refresh();
     }
 }
