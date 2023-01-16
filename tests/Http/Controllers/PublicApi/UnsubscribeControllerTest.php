3b196b424c96a991b30dce76cea64ba2ffa34bdb
Rias <hey@rias.be>
2023-01-16 12:25:52 +0100

    Remove unnecessary tests + fix existing

--- tests/Http/Controllers/PublicApi/UnsubscribeControllerTest.php
+++ tests/Http/Controllers/PublicApi/UnsubscribeControllerTest.php
@@ -17,30 +17,5 @@ beforeEach(function () {
 it('can load the unsubscribe url without a send', function () {
     $this
         ->get(action([UnsubscribeController::class, 'show'], test()->subscriber->uuid))
-        ->assertSee('Are you sure you want to unsubscribe from list')
-        ->assertSee('document.getElementById');
-});
-
-it('can load the unsubscribe url with a send and wont automatically confirm right away', function () {
-    $send = Send::factory()->create([
-        'subscriber_id' => test()->subscriber->id,
-        'created_at' => now(),
-    ]);
-
-    $this
-        ->get(action([UnsubscribeController::class, 'show'], [test()->subscriber->uuid, $send->uuid]))
-        ->assertSee('Are you sure you want to unsubscribe from list')
-        ->assertDontSee('document.getElementById');
-});
-
-it('can load the unsubscribe url with a send and will automatically confirm after 5 minutes', function () {
-    $send = Send::factory()->create([
-        'subscriber_id' => test()->subscriber->id,
-        'created_at' => now()->subMinutes(5),
-    ]);
-
-    $this
-        ->get(action([UnsubscribeController::class, 'show'], [test()->subscriber->uuid, $send->uuid]))
-        ->assertSee('Are you sure you want to unsubscribe from list')
-        ->assertSee('document.getElementById');
+        ->assertSee('Are you sure you want to unsubscribe from list');
 });
