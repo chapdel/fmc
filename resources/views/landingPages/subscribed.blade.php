ba1e4c9bd02e546a7c92016ca651d27341668c29
Rias <hey@rias.be>
2023-01-26 09:02:40 +0100

    Handle honeypot

--- resources/views/landingPages/subscribed.blade.php
+++ resources/views/landingPages/subscribed.blade.php
@@ -5,8 +5,10 @@
         {{ __mc('Happy to have you!') }}
     </p>
     <p class="mt-4">
-        @isset($subscriber)
+        @if(isset($subscriber) && $subscriber->emailList)
             {!! __mc('You are now subscribed to the list <strong class="font-semibold">:emailListName</strong>.', ['emailListName' => $subscriber->emailList->name]) !!}
+        @elseif (isset($subscriber))
+            {!! __mc('You are now subscribed.') !!}
         @else
             {!! __mc('You are now subscribed to the list <strong class="font-semibold">our dummy mailing list</strong>.') !!}
         @endif
