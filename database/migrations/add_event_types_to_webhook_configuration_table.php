34b58f5998f796a6533c57ee2ceee53af81ca3ce
Tim Van Dijck <tim.van.dijck@gmail.com>
2023-03-10 11:16:53 +0100

    Fix typo

--- database/migrations/add_event_types_to_webhook_configuration_table.php
+++ database/migrations/add_event_types_to_webhook_configuration_table.php
@@ -9,7 +9,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('mailcoach_webhook_configurations', function (Blueprint $table) {
-            $table->boolean('enabled')->default(true)->after('id')
+            $table->boolean('enabled')->default(true)->after('id');
             $table->boolean('use_for_all_events')->default(true)->after('use_for_all_lists');
             $table->json('events')->nullable()->after('use_for_all_events');
         });
