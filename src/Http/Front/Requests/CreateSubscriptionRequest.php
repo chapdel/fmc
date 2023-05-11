30ee8645513c9f32e24f5a1f280790d650cd9b3e
Rias <hey@rias.be>
2023-05-11 15:56:14 +0200

    Add turnstile support

--- src/Http/Front/Requests/CreateSubscriptionRequest.php
+++ src/Http/Front/Requests/CreateSubscriptionRequest.php
@@ -2,8 +2,11 @@
 
 namespace Spatie\Mailcoach\Http\Front\Requests;
 
+use Closure;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Validator;
 use Spatie\Mailcoach\Domain\Audience\Models\EmailList;
 use Spatie\Mailcoach\Domain\Shared\Traits\UsesMailcoachModels;
 
@@ -77,5 +80,38 @@ class CreateSubscriptionRequest extends FormRequest
         }
 
         return $this->emailList;
+    }
+
+    public function requiresTurnstile(): bool
+    {
+        return !empty(config('mailcoach.turnstile_secret'));
+    }
+
+    public function hasTurnstileResponse(): bool
+    {
+        return $this->has('cf-turnstile-response');
+    }
+
+    public function validateTurnstile(): void
+    {
+        Validator::make([
+            'cf-turnstile-response' => $this->get('cf-turnstile-response'),
+        ], [
+            'cf-turnstile-response' => ['required', function (string $attribute, mixed $value, Closure $fail) {
+                if (! app()->environment('production')) {
+                    return;
+                }
+
+                $success = Http::post('https://challenges.cloudflare.com/turnstile/v0/siteverify', [
+                    'secret' => config('services.turnstile.secret'),
+                    'response' => $value,
+                    'remoteip' => $this->ip(),
+                ])->json('success');
+
+                if (! $success) {
+                    $fail(__mc('The turnstile validation failed.'));
+                }
+            }]
+        ])->validate();
     }
 }
