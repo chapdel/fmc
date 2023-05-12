70d19fb752c6d38bc7568d5d3852296825a7a9bc
Rias <hey@rias.be>
2023-05-12 09:31:37 +0200

    Return an error message when the campaign can't be sent

--- src/Http/Api/Controllers/Campaigns/SendCampaignController.php
+++ src/Http/Api/Controllers/Campaigns/SendCampaignController.php
@@ -3,6 +3,7 @@
 namespace Spatie\Mailcoach\Http\Api\Controllers\Campaigns;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Spatie\Mailcoach\Domain\Campaign\Exceptions\CouldNotSendCampaign;
 use Spatie\Mailcoach\Domain\Campaign\Models\Campaign;
 use Spatie\Mailcoach\Http\Api\Controllers\Concerns\RespondsToApiRequests;
 use Spatie\Mailcoach\Http\Api\Requests\SendCampaignRequest;
@@ -16,7 +17,11 @@ class SendCampaignController
     {
         $this->authorize('update', $campaign);
 
-        $campaign->send();
+        try {
+            $campaign->send();
+        } catch (CouldNotSendCampaign $exception) {
+            abort(400, $exception->getMessage());
+        }
 
         return $this->respondOk();
     }
