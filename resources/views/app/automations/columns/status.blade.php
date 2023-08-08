f66372b3f6ff3446a12bf1f940617d542b50e85a
Rias <hey@rias.be>
2023-08-08 09:01:28 +0200

    Add tooltips

--- resources/views/app/automations/columns/status.blade.php
+++ resources/views/app/automations/columns/status.blade.php
@@ -4,12 +4,12 @@
     <button class="group" wire:click.prevent="toggleAutomationStatus({{ $automation->id }})">
         @if($automation->status === \Spatie\Mailcoach\Domain\Automation\Enums\AutomationStatus::Paused)
             <span class="group-hover:opacity-0 fas fa-magic text-gray-400"></span>
-            <span title="{{ __mc('Start Automation') }}" class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100">
+            <span x-data x-tooltip="'{{ __mc('Start Automation') }}'" class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100">
                 <x-mailcoach::rounded-icon class="w-5 h-5 relative -top-[2px]" type="success" icon="fas fa-play"/>
             </span>
         @else
             <span class="group-hover:opacity-0 fas fa-sync fa-spin text-green-500"></span>
-            <span title="{{ __mc('Pause Automation') }}"
+            <span x-data x-tooltip="'{{ __mc('Pause Automation') }}'"
                   class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100">
                 <x-mailcoach::rounded-icon class="w-5 h-5 relative -top-[2px]" type="warning" icon="fas fa-pause"/>
             </span>
