fcae7cd83295fdc571ca097db2599cc0d7b71ba8
Rias <hey@rias.be>
2023-08-08 08:54:48 +0200

    Add tooltip

--- resources/views/app/emailLists/columns/name.blade.php
+++ resources/views/app/emailLists/columns/name.blade.php
@@ -3,7 +3,7 @@
 <div class="link fi-ta-text-item inline-flex items-center gap-1.5 px-3 text-sm">
     {{ $emailList->name }}
     @if ($emailList->websiteEnabled())
-        <a class="link text-xs" title="{{ __mc('Website') }}" href="{{ $emailList->websiteUrl() }}" target="_blank">
+        <a class="link text-xs" x-data="{}" x-tooltip="'{{ __mc('View website') }}'" title="{{ __mc('Website') }}" href="{{ $emailList->websiteUrl() }}" target="_blank">
             <i class="fas fa-external-link"></i>
         </a>
     @endif
