2496f85a23483744708070c17ffeb1a557e0111a
Niels Vanpachtenbeke <10651054+Nielsvanpach@users.noreply.github.com>
2023-08-24 13:54:50 +0200

    provide empty campaign in factory

--- database/factories/CampaignFactory.php
+++ database/factories/CampaignFactory.php
@@ -23,12 +23,36 @@ class CampaignFactory extends Factory
             'subject' => $this->faker->sentence,
             'from_email' => $this->faker->email,
             'from_name' => $this->faker->name,
-            'html' => $this->faker->randomHtml(),
+            'html' => $html = $this->faker->randomHtml(),
+            'structured_html' => json_encode([
+                'templateValues' => [
+                    'html' => json_encode($html),
+                ],
+            ], JSON_THROW_ON_ERROR),
             'status' => CampaignStatus::Draft,
             'uuid' => $this->faker->uuid,
             'last_modified_at' => now(),
             'email_list_id' => EmailList::factory(),
             'template_id' => Template::factory(),
         ];
+    }
+
+    public function emptyDraft(string $templateId = null): Factory
+    {
+        return $this->state(function (array $attributes) use ($templateId) {
+            return [
+                'from_email' => null,
+                'from_name' => null,
+                'reply_to_email' => null,
+                'reply_to_name' => null,
+                'template_id' => $templateId,
+                'html' => null,
+                'structured_html' => json_encode([
+                    'templateValues' => [
+                        'html' => null,
+                    ],
+                ], JSON_THROW_ON_ERROR),
+            ];
+        });
     }
 }
