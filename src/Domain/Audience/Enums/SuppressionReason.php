44b704a90b3772a992232279a62d6ce2e8e0f7c4
Niels Vanpachtenbeke <10651054+Nielsvanpach@users.noreply.github.com>
2023-08-31 12:16:37 +0200

    use dev-main of ses-feedback

--- src/Domain/Audience/Enums/SuppressionReason.php
+++ src/Domain/Audience/Enums/SuppressionReason.php
@@ -7,14 +7,12 @@ use Filament\Support\Contracts\HasLabel;
 enum SuppressionReason: string implements HasLabel
 {
     case hardBounce = 'hard_bounce';
-    case spamComplaint = 'spam_complaint';
     case manual = 'manual';
 
     public function getLabel(): ?string
     {
         return match ($this) {
             self::hardBounce => __mc('Hard bounce'),
-            self::spamComplaint => __mc('Spam complaint'),
             self::manual => __mc('Manual'),
         };
     }
