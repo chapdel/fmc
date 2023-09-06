8edb8b69ef70238bbcacf4f2ca10dbfb8732895c
Rias <hey@rias.be>
2023-09-06 10:35:20 +0200

    Refactor suppression check action

--- src/Domain/Shared/Actions/EnsureEmailsNotOnSuppressionListAction.php
+++ src/Domain/Shared/Actions/EnsureEmailsNotOnSuppressionListAction.php
@@ -2,7 +2,7 @@
 
 namespace Spatie\Mailcoach\Domain\Shared\Actions;
 
-use InvalidArgumentException;
+use Illuminate\Support\Arr;
 use Spatie\Mailcoach\Domain\Shared\Traits\UsesMailcoachModels;
 use Spatie\Mailcoach\Domain\TransactionalMail\Exceptions\SuppressedEmail;
 
@@ -12,23 +12,8 @@ class EnsureEmailsNotOnSuppressionListAction
 
     public function execute(string|array $email): void
     {
-        if (is_array($email)) {
-            $this->ensureMultipleEmails($email);
+        $emails = Arr::wrap($email);
 
-            return;
-        }
-
-        if (! filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            throw new InvalidArgumentException("The given email `{$email}` is invalid.");
-        }
-
-        if (self::getSuppressionClass()::where('email', $email)->exists()) {
-            throw SuppressedEmail::make($email);
-        }
-    }
-
-    protected function ensureMultipleEmails(array $emails): void
-    {
         $firstSuppressed = self::getSuppressionClass()::query()
             ->whereIn('email', $emails)
             ->first();
