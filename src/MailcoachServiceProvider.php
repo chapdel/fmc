724a34a329e70429c03a9fb06fa49ba7747f6176
Niels Vanpachtenbeke <10651054+Nielsvanpach@users.noreply.github.com>
2023-09-19 14:38:02 +0200

    remove commands again

--- src/MailcoachServiceProvider.php
+++ src/MailcoachServiceProvider.php
@@ -58,8 +58,6 @@ use Spatie\Mailcoach\Domain\Campaign\Listeners\AddCampaignClickedTag;
 use Spatie\Mailcoach\Domain\Campaign\Listeners\AddCampaignOpenedTag;
 use Spatie\Mailcoach\Domain\Campaign\Listeners\SendCampaignSentEmail;
 use Spatie\Mailcoach\Domain\Campaign\Listeners\SetWebhookCallProcessedAt;
-use Spatie\Mailcoach\Domain\Settings\Commands\MakeUserCommand;
-use Spatie\Mailcoach\Domain\Settings\Commands\PrepareGitIgnoreCommand;
 use Spatie\Mailcoach\Domain\Settings\Commands\PublishCommand;
 use Spatie\Mailcoach\Domain\Settings\EventSubscribers\WebhookEventSubscriber;
 use Spatie\Mailcoach\Domain\Settings\EventSubscribers\WebhookFailedAttemptsSubscriber;
@@ -227,8 +225,6 @@ class MailcoachServiceProvider extends PackageServiceProvider
                 CheckLicenseCommand::class,
                 DeleteOldExportsCommand::class,
                 PublishCommand::class,
-                MakeUserCommand::class,
-                PrepareGitIgnoreCommand::class,
             ]);
     }
 
