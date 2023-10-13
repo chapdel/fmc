ba4608f0d473f10464e2467e8a2f89403f4e65c1
Rias <hey@rias.be>
2023-10-13 11:17:25 +0200

    Fix embedded webview with wire:navigate

--- resources/views/app/components/webview.blade.php
+++ resources/views/app/components/webview.blade.php
@@ -1,24 +1,34 @@
 @if(isset($src) || isset($html))
-    <embedded-webview @isset($src) src="{{ $src }}" @endisset>
-    </embedded-webview>
+    @php($uuid = \Illuminate\Support\Str::uuid())
+    @php($key = \Illuminate\Support\Str::random())
 
+    <embedded-webview-{{ $uuid }} @isset($src) src="{{ $src }}" @endisset>
+    </embedded-webview-{{ $uuid }}>
+
+    @isset($html)
     <script>
-        class EmbeddedWebview extends HTMLElement {
+        class EmbeddedWebview{{ $key }} extends HTMLElement {
             connectedCallback() {
-                @isset($html)
-                    const shadow = this.attachShadow({ mode: 'closed' });
-                    shadow.innerHTML = @json($html);
-                @else
+                const shadow = this.attachShadow({ mode: 'closed' });
+                shadow.innerHTML = @js(str_contains($html, '<html') ? $html : "<pre>{$html}</pre>");
+            }
+        }
+    </script>
+    @else
+        <script>
+            class EmbeddedWebview{{ $key }} extends HTMLElement {
+                connectedCallback() {
                     fetch(this.getAttribute('src'))
                     .then(response => response.text())
                     .then(html => {
                         const shadow = this.attachShadow({ mode: 'closed' });
                         shadow.innerHTML = html;
                     });
-                @endisset
+                }
             }
-        }
-
-        window.customElements.define('embedded-webview', EmbeddedWebview);
+        </script>
+    @endif
+    <script>
+        window.customElements.define('embedded-webview-{{ $uuid }}', EmbeddedWebview{{ $key }});
     </script>
 @endif
