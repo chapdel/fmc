2fbedcc23635ca9647fd8e92a4d004d2eada5ebf
Rias <hey@rias.be>
2023-10-24 10:50:10 +0200

    Fix intersection observer

--- resources/views/app/components/form/formButtons.blade.php
+++ resources/views/app/components/form/formButtons.blade.php
@@ -1,12 +1,11 @@
-@php($id = \Illuminate\Support\Str::random(4))
-<div id="form-buttons-{{ $id }}" class="form-buttons {{ $attributes->get('class') }}" {{ $attributes->except('class') }}>
+<div
+    x-data="{
+        stuck: false,
+    }"
+    x-intersect:enter.threshold.full="stuck = false"
+    x-intersect:leave.threshold.full="stuck = true"
+    :class="stuck ? 'form-buttons-stuck' : ''"
+    class="form-buttons {{ $attributes->get('class') }}" {{ $attributes->except('class') }}
+>
     {{ $slot }}
 </div>
-
-<script>
-  const observer{{ $id }} = new IntersectionObserver(
-    ([e]) => e.target.classList.toggle('form-buttons-stuck', e.intersectionRatio < 1),
-    {threshold: [1]}
-  );
-  observer{{ $id }}.observe(document.querySelector('#form-buttons-{{ $id }}'))
-</script>
