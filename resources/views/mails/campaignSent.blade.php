478143c3a178301c56b08233a15cde955ce1eb25
Rias <hey@rias.be>
2023-10-31 10:29:58 +0100

    Fix campaign sent notification

--- resources/views/mails/campaignSent.blade.php
+++ resources/views/mails/campaignSent.blade.php
@@ -1,7 +1,12 @@
+<?php /** @var \Spatie\Mailcoach\Domain\Campaign\Models\Campaign $campaign */ ?>
 @component('mailcoach::mails.layout.message')
 {{ __mc('Good job!') }}
 
-{{ __mc('Campaign **:campaignName** was sent to **:numberOfSubscribers** subscribers (list :emailListName)',['campaignName'=>$campaign->name,'numberOfSubscribers'=>($campaign->sent_to_number_of_subscribers ?? 0 ),'emailListName'=>$campaign->emailList->name]) }}.
+{{ __mc('Campaign **:campaignName** was sent to **:numberOfSubscribers** subscribers (list :emailListName)',[
+    'campaignName' => $campaign->name,
+    'numberOfSubscribers' => ($campaign->sentToNumberOfSubscribers() ?? 0 ),
+    'emailListName' => $campaign->emailList->name]
+) }}.
 
 @component('mailcoach::mails.layout.button', ['url' => $summaryUrl])
 {{ __mc('View summary') }}
