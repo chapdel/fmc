7022685540b14ba3b7b40dec4e7c96c2263d0a5d
Rias <hey@rias.be>
2023-11-06 14:38:57 +0100

    Add backwards compatible endpoint for segments

--- src/Http/Api/Controllers/EmailLists/SegmentsController.php
+++ src/Http/Api/Controllers/EmailLists/SegmentsController.php
@@ -8,6 +8,7 @@ use Spatie\Mailcoach\Domain\Audience\Models\TagSegment;
 use Spatie\Mailcoach\Domain\Shared\Traits\UsesMailcoachModels;
 use Spatie\Mailcoach\Http\Api\Controllers\Concerns\RespondsToApiRequests;
 use Spatie\Mailcoach\Http\Api\Queries\SegmentsQuery;
+use Spatie\Mailcoach\Http\Api\Requests\TagSegmentRequest;
 use Spatie\Mailcoach\Http\Api\Resources\SegmentResource;
 
 class SegmentsController
@@ -34,6 +35,96 @@ class SegmentsController
         $segment->load('emailList');
 
         return new SegmentResource($segment);
+    }
+
+    public function store(EmailList $emailList, TagSegmentRequest $request)
+    {
+        $tagClass = self::getTagSegmentClass();
+
+        $this->authorize('create', $tagClass);
+
+        $positiveTags = self::getTagClass()::query()
+            ->where('email_list_id', $emailList->id)
+            ->whereIn('name', $request->validated('positive_tags'))
+            ->pluck('id');
+
+        $negativeTags = self::getTagClass()::query()
+            ->where('email_list_id', $emailList->id)
+            ->whereIn('name', $request->validated('negative_tags'))
+            ->pluck('id');
+
+        $storedConditions = [];
+
+        if ($positiveTags->count()) {
+            $storedConditions[] = [
+                'value' => $positiveTags->values()->toArray(),
+                'condition_key' => 'subscriber_tags',
+                'comparison_operator' => $request->validated('all_positive_tags_required')
+                    ? 'all'
+                    : 'in',
+            ];
+        }
+
+        if ($negativeTags->count()) {
+            $storedConditions[] = [
+                'value' => $negativeTags->values()->toArray(),
+                'condition_key' => 'subscriber_tags',
+                'comparison_operator' => $request->validated('all_negative_tags_required')
+                    ? 'none'
+                    : 'not-in',
+            ];
+        }
+
+        $segment = $emailList->segments()->create([
+            'name' => $request->validated('name'),
+            'stored_conditions' => $storedConditions,
+        ]);
+
+        return SegmentResource::make($segment);
+    }
+
+    public function update(EmailList $emailList, TagSegment $segment, TagSegmentRequest $request)
+    {
+        $this->authorize('update', $segment);
+
+        $positiveTags = self::getTagClass()::query()
+            ->where('email_list_id', $emailList->id)
+            ->whereIn('name', $request->validated('positive_tags'))
+            ->pluck('id');
+
+        $negativeTags = self::getTagClass()::query()
+            ->where('email_list_id', $emailList->id)
+            ->whereIn('name', $request->validated('negative_tags'))
+            ->pluck('id');
+
+        $storedConditions = [];
+
+        if ($positiveTags->count()) {
+            $storedConditions[] = [
+                'value' => $positiveTags->values()->toArray(),
+                'condition_key' => 'subscriber_tags',
+                'comparison_operator' => $request->validated('all_positive_tags_required')
+                    ? 'all'
+                    : 'in',
+            ];
+        }
+
+        if ($negativeTags->count()) {
+            $storedConditions[] = [
+                'value' => $negativeTags->values()->toArray(),
+                'condition_key' => 'subscriber_tags',
+                'comparison_operator' => $request->validated('all_negative_tags_required')
+                    ? 'none'
+                    : 'not-in',
+            ];
+        }
+
+        $segment->update([
+            'name' => $request->validated('name'),
+            'stored_conditions' => $storedConditions,
+        ]);
+
+        return SegmentResource::make($segment);
     }
 
     public function destroy(EmailList $emailList, TagSegment $segment)
