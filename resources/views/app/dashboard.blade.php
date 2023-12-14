709f43f071ed8dd72be34669691916a65629bf5f
Rias <hey@rias.be>
2023-12-14 10:05:49 +0100

    Don't show queue error on dashboard when using vapor

--- resources/views/app/dashboard.blade.php
+++ resources/views/app/dashboard.blade.php
@@ -29,7 +29,7 @@ $horizonStatus = app(\Spatie\Mailcoach\Domain\Shared\Support\HorizonStatus::clas
             </x-mailcoach::tile>
         @endif
 
-        @if (! $hasQueueConnection)
+        @if (!$usesVapor && ! $hasQueueConnection)
             <x-mailcoach::tile danger cols="2" icon="database" link="https://mailcoach.app/docs" target="_blank">
                 {!! __mc('<h2 class="dashboard-title">Queue error</h2> <p>No connection found. Configure a queue connection with the <code>mailcoach-redis</code> key.</p>') !!}
             </x-mailcoach::tile>
