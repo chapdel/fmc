831edd811520fed40cf1a7345f708e26ac28f828
Rias <hey@rias.be>
2023-12-14 11:00:26 +0100

    Make Unlayer play nice with split testing

--- resources/views/editors/unlayer/unlayer.blade.php
+++ resources/views/editors/unlayer/unlayer.blade.php
@@ -1,13 +1,6 @@
-@push('endHead')
-    <style>
-        #unlayer-wrapper {
-            margin-top: 0;
-        }
-    </style>
-@endpush
-<div id="unlayer-wrapper">
+<div class="mt-0">
     <script>
-        function loadTemplate() {
+        function loadTemplate{{ $model->id }}() {
             document.getElementById('unlayer_template_error').classList.add('hidden');
             let slug = document.getElementById('unlayer_template').value;
             slug = slug.split('/').slice(-1)[0];
@@ -36,7 +29,7 @@
                 .then((result) => {
                     if (! result.data.StockTemplate) {
                         @if (config('mailcoach.unlayer.options.projectId'))
-                        unlayer.loadTemplate(slug);
+                        window.editor{{ $model->id }}.loadTemplate(slug);
                         Alpine.store('modals').close('load-unlayer-template');
                         @else
                         document.getElementById('unlayer_template_error').innerHTML = '{{ __mc('Template not found.') }}';
@@ -46,25 +39,30 @@
                         return;
                     }
 
-                    unlayer.loadDesign(result.data.StockTemplate.StockTemplatePages[0].design);
-                    Alpine.store('modals').close('load-unlayer-template');
+                    window.editor{{ $model->id }}.loadDesign(result.data.StockTemplate.StockTemplatePages[0].design);
+                    window.dispatchEvent(new CustomEvent('close-modal', { detail: { id: 'load-unlayer-template-{{ $model->id }}' }}))
                 });
         }
 
-        window.init = function() {
-            document.getElementById('load-template').addEventListener('click', loadTemplate);
+        window.init{{ $model->id }} = function() {
+            document.getElementById('load-template-{{ $model->id }}').addEventListener('click', loadTemplate{{ $model->id }});
 
-            unlayer.init(@json($options));
+            let options = @js($options);
+            options.id = 'editor{{ $model->id }}';
 
-            unlayer.loadDesign(JSON.parse(JSON.stringify(this.json).replaceAll('[[[', '&#91;&#91;&#91;')));
+            const editor = unlayer.createEditor(options);
+
+            window.editor{{ $model->id }} = editor;
+
+            editor.loadDesign(JSON.parse(JSON.stringify(this.json).replaceAll('[[[', '&#91;&#91;&#91;')));
 
             if (! this.json) {
-                unlayer.loadBlank({
+                editor.loadBlank({
                     backgroundColor: '#ffffff'
                 });
             }
 
-            unlayer.registerCallback('image', (file, done) => {
+            editor.registerCallback('image', (file, done) => {
                 let data = new FormData();
                 data.append('file', file.attachments[0]);
 
@@ -95,18 +93,18 @@
                 ];
             })->toArray());
 
-            unlayer.setMergeTags(mergeTags);
+            editor.setMergeTags(mergeTags);
 
             const component = this;
-            unlayer.addEventListener('design:updated', () => {
-                unlayer.exportHtml(function(data) {
+            editor.addEventListener('design:updated', () => {
+                editor.exportHtml(function(data) {
                     component.html = data.html;
                     component.json = JSON.parse(JSON.stringify(data.design));
                 });
             });
 
-            unlayer.addEventListener('design:loaded', function(data) {
-                unlayer.exportHtml(function(data) {
+            editor.addEventListener('design:loaded', function(data) {
+                editor.exportHtml(function(data) {
                     component.html = data.html;
                     component.json = data.design;
                 });
@@ -118,9 +116,9 @@
         <div wire:ignore x-data="{
             html: @entangle('templateFieldValues.html').live,
             json: @entangle('templateFieldValues.json').live,
-            init: init,
+            init: init{{ $model->id }},
         }" class="overflow-hidden flex-1 h-full mb-6">
-            <div id="editor" class="h-full pr-3 py-1" style="min-height: 75vh; height: 75vh"></div>
+            <div id="editor{{ $model->id }}" class="h-full pr-3 py-1" style="min-height: 75vh; height: 75vh"></div>
         </div>
 
         @isset($errors)
@@ -129,12 +127,12 @@
             @enderror
         @endisset
 
-        <x-mailcoach::button-secondary x-on:click.prevent="$store.modals.open('load-unlayer-template')" :label="__mc('Load Unlayer template')"/>
+        <x-mailcoach::button-secondary x-on:click.prevent="$dispatch('open-modal', { id: 'load-unlayer-template-{{ $model->id }}' })" :label="__mc('Load Unlayer template')"/>
     </div>
 </div>
 
 @push('modals')
-    <x-mailcoach::modal :title="__mc('Load Unlayer template')" name="load-unlayer-template">
+    <x-mailcoach::modal :title="__mc('Load Unlayer template')" name="load-unlayer-template-{{ $model->id }}">
         <p>{!! __mc('You can load an <a class="text-blue-500" href="https://unlayer.com/templates" target="_blank">Unlayer template</a> by entering the URL') !!}</p>
         @if(config('mailcoach.unlayer.options.projectId'))
             <p>{{ __mc('A template id from your Unlayer project also works') }}</p>
@@ -146,8 +144,8 @@
         </div>
 
         <div class="form-buttons">
-            <x-mailcoach::button class="mt-auto" id="load-template" label="Load" type="button" />
-            <x-mailcoach::button-cancel x-on:click.prevent="$store.modals.close('load-unlayer-template')" :label=" __mc('Cancel')" />
+            <x-mailcoach::button class="mt-auto" id="load-template-{{ $model->id }}" label="Load" type="button" />
+            <x-mailcoach::button-cancel x-on:click.prevent="$dispatch('close-modal', { id: 'load-unlayer-template-{{ $model->id }}' })" :label=" __mc('Cancel')" />
         </div>
     </x-mailcoach::modal>
 @endpush
