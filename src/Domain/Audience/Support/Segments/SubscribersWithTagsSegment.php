739a696c16b2e1f13f589d99825061a41007bab8
Rias <hey@rias.be>
2023-12-14 14:54:36 +0100

    Make sure segments filter on the email list id

--- src/Domain/Audience/Support/Segments/SubscribersWithTagsSegment.php
+++ src/Domain/Audience/Support/Segments/SubscribersWithTagsSegment.php
@@ -4,9 +4,12 @@ namespace Spatie\Mailcoach\Domain\Audience\Support\Segments;
 
 use Illuminate\Database\Eloquent\Builder;
 use Spatie\Mailcoach\Domain\Audience\Models\TagSegment as TagSegmentModel;
+use Spatie\Mailcoach\Domain\Shared\Traits\UsesMailcoachModels;
 
 class SubscribersWithTagsSegment extends Segment
 {
+    use UsesMailcoachModels;
+
     public function description(): string
     {
         if ($this->getTagSegmentModel()) {
@@ -25,6 +28,8 @@ class SubscribersWithTagsSegment extends Segment
         }
 
         $tagSegment->applyConditionBuilder($subscribersQuery);
+
+        $subscribersQuery->where(self::getSubscriberTableName().'.email_list_id', $tagSegment->email_list_id);
     }
 
     public function getTagSegmentModel(): ?TagSegmentModel
