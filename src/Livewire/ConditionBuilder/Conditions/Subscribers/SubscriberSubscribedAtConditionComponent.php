74044f10bd49d8f60f451e5c8113a7e47622b171
Rias <hey@rias.be>
2023-12-15 09:34:42 +0100

    Reset value to a correct default when switching from before/after to between and back

--- src/Livewire/ConditionBuilder/Conditions/Subscribers/SubscriberSubscribedAtConditionComponent.php
+++ src/Livewire/ConditionBuilder/Conditions/Subscribers/SubscriberSubscribedAtConditionComponent.php
@@ -2,10 +2,22 @@
 
 namespace Spatie\Mailcoach\Livewire\ConditionBuilder\Conditions\Subscribers;
 
+use Spatie\Mailcoach\Domain\ConditionBuilder\Enums\ComparisonOperator;
 use Spatie\Mailcoach\Livewire\ConditionBuilder\ConditionComponent;
 
 class SubscriberSubscribedAtConditionComponent extends ConditionComponent
 {
+    public function updatedStoredCondition($newValue, $property)
+    {
+        if ($property === 'comparison_operator' && $newValue === ComparisonOperator::Between->value) {
+            $this->storedCondition['value'] = [];
+        }
+
+        if ($property === 'comparison_operator' && $newValue !== ComparisonOperator::Between->value) {
+            $this->storedCondition['value'] = '';
+        }
+    }
+
     public function render()
     {
         return view('mailcoach::app.conditionBuilder.conditions.subscribers.subscriberSubscribedAtCondition');
