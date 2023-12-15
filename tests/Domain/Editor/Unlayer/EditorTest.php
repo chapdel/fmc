ec83e057fc988052daf61e5f349e6dd653687dda
Rias <hey@rias.be>
2023-12-15 10:20:32 +0100

    Fix Unlayer test

--- tests/Domain/Editor/Unlayer/EditorTest.php
+++ tests/Domain/Editor/Unlayer/EditorTest.php
@@ -6,7 +6,7 @@ use Spatie\Mailcoach\Domain\Template\Models\Template;
 it('can render a view', function () {
     $template = Template::factory()->create();
     Livewire::test('mailcoach::editor-unlayer', ['model' => $template])
-        ->assertSee('unlayer.init');
+        ->assertSee('unlayer.createEditor');
 });
 
 test('passes along configured options', function () {
