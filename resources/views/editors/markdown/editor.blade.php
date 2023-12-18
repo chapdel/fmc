bfded8fcd57422c4d5947e4bec0654c102b53834
Rias <hey@rias.be>
2023-12-18 14:38:08 +0100

    Markdown editor improvements

--- resources/views/editors/markdown/editor.blade.php
+++ resources/views/editors/markdown/editor.blade.php
@@ -1,74 +1,77 @@
+@assets
+<style>
+    /* Override the styles set by Filament for EasyMDE */
+    .EasyMDEContainer .editor-toolbar button:before {
+        -webkit-mask-image: none !important;
+        mask-image: none !important;
+        display: none;
+        content: '';
+    }
+
+    .cm-s-easymde .cm-header-1 {
+        font-size: 1.875rem
+    }
+
+    .cm-s-easymde .cm-header-2 {
+        font-size: 1.5rem
+    }
+
+    .cm-s-easymde .cm-header-3 {
+        font-size: 1.25rem
+    }
+
+    .cm-s-easymde .cm-header-4 {
+        font-size: 1.125rem
+    }
+
+    .cm-s-easymde .cm-header-5 {
+        font-size:1.125rem
+    }
+
+    .cm-s-easymde .cm-header-6 {
+        font-size:1rem
+    }
+
+    .cm-s-easymde .cm-comment {
+        background: none;
+    }
+
+    .cm-keyword {color: #708;}
+    .cm-atom {color: #219;}
+    .cm-number {color: #164;}
+    .cm-def {color: #00f;}
+    .cm-variable,
+    .cm-punctuation,
+    .cm-property,
+    .cm-operator {}
+    .cm-variable-2 {color: #05a;}
+    .cm-formatting-list, .cm-formatting-list + .cm-variable-2 {color: #000;}
+    .cm-variable-3, .cm-s-default .cm-type {color: #085;}
+    .cm-comment {color: #a50;}
+    .cm-string {color: #a11;}
+    .cm-string-2 {color: #f50;}
+    .cm-meta {color: #555;}
+    .cm-qualifier {color: #555;}
+    .cm-builtin {color: #30a;}
+    .cm-bracket {color: #997;}
+    .cm-tag {color: #170;}
+    .cm-attribute {color: #00c;}
+    .cm-hr {color: #999;}
+    .cm-link {color: #00c;}
+</style>
+<script>
+    window.debounce = function (func, timeout = 300){
+        let timer;
+        return (...args) => {
+            clearTimeout(timer);
+            timer = setTimeout(() => { func.apply(this, args); }, timeout);
+        };
+    }
+</script>
+@endassets
 <div class="form-grid">
-    <style>
-        /* Override the styles set by Filament for EasyMDE */
-        .EasyMDEContainer .editor-toolbar button:before {
-            -webkit-mask-image: none !important;
-            mask-image: none !important;
-            display: none;
-            content: '';
-        }
-
-        .cm-s-easymde .cm-header-1 {
-            font-size: 1.875rem
-        }
-
-        .cm-s-easymde .cm-header-2 {
-            font-size: 1.5rem
-        }
-
-        .cm-s-easymde .cm-header-3 {
-            font-size: 1.25rem
-        }
-
-        .cm-s-easymde .cm-header-4 {
-            font-size: 1.125rem
-        }
-
-        .cm-s-easymde .cm-header-5 {
-            font-size:1.125rem
-        }
-
-        .cm-s-easymde .cm-header-6 {
-            font-size:1rem
-        }
-
-        .cm-s-easymde .cm-comment {
-            background: none;
-        }
-
-        .cm-keyword {color: #708;}
-        .cm-atom {color: #219;}
-        .cm-number {color: #164;}
-        .cm-def {color: #00f;}
-        .cm-variable,
-        .cm-punctuation,
-        .cm-property,
-        .cm-operator {}
-        .cm-variable-2 {color: #05a;}
-        .cm-formatting-list, .cm-formatting-list + .cm-variable-2 {color: #000;}
-        .cm-variable-3, .cm-s-default .cm-type {color: #085;}
-        .cm-comment {color: #a50;}
-        .cm-string {color: #a11;}
-        .cm-string-2 {color: #f50;}
-        .cm-meta {color: #555;}
-        .cm-qualifier {color: #555;}
-        .cm-builtin {color: #30a;}
-        .cm-bracket {color: #997;}
-        .cm-tag {color: #170;}
-        .cm-attribute {color: #00c;}
-        .cm-hr {color: #999;}
-        .cm-link {color: #00c;}
-    </style>
     <script>
-        function debounce(func, timeout = 300){
-            let timer;
-            return (...args) => {
-                clearTimeout(timer);
-                timer = setTimeout(() => { func.apply(this, args); }, timeout);
-            };
-        }
-
-        window.init = function() {
+        window.init{{ $model->id }} = function() {
             let editor = new EasyMDE({
                 autoDownloadFontAwesome: false,
                 element: this.$refs.editor,
@@ -209,7 +212,7 @@
                     html: @entangle('templateFieldValues.' . $field['name'] . '.html').live,
                     markdown: @entangle('templateFieldValues.' . $field['name'] . '.markdown').live,
                     theme: @entangle('templateFieldValues.' . $field['name'] . '.theme').live,
-                    init: init,
+                    init: init{{ $model->id }},
                 }">
                     <textarea x-ref="editor"></textarea>
 
